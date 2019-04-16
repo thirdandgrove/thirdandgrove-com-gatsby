192f55dd47d45126fcd1c4a9fd56265f6d929dfc
Grant Glidewell <grant@thirdandgrove.com>
2019-04-16 13:23:44 -0700

    added: stubbed footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { css } from '@emotion/core';
 
-import SiteNav from './SiteNav';
+import SiteNav from '../SiteNav';
 
 const Header = ({ defaultBackground, tagline }) => (
   <header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Header from '../components/header';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default () => {
   const tagline = () => {
     const options = [
       'We are an obsessive digital innovation company.',
       'Design-first technologists who help innovative brands make their next move.',
+      'Some other amazing quote about TAG.',
     ];
     return options[Math.floor(Math.random() * options.length)];
   };
@@ -14,6 +16,7 @@ export default () => {
     <>
       <Header tagline={tagline()} />
       <div>content here</div>
+      <Footer />
     </>
   );
 };
