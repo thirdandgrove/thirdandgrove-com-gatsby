233eb0490dc606bc12188a786bfbf1ce436e83f2
Grant Glidewell <grant@thirdandgrove.com>
2019-04-23 09:36:54 -0700

    moved react hook into hooks folder, updated standards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import useInterval from './hooks/useInterval';
+import useInterval from '../hooks/useInterval';
 
 import Layout from '../components/layout';
 
