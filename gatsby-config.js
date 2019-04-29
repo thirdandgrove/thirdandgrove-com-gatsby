55ed6aca366b1e949610a061fea58444efe90573
Grant Glidewell <grant@thirdandgrove.com>
2019-04-29 13:25:52 -0700

    using static dir for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images`,
       },
     },
     `gatsby-transformer-sharp`,
