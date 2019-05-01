3b312edc075d9b24ef12f968510c5422e2ea79b5
Grant Glidewell <grant@thirdandgrove.com>
2019-05-01 08:20:26 -0700

    extracted hamburger and close svgs

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -6,6 +6,8 @@ import useWindow from '../../hooks/useWindow';
 import Menu from '../Menu';
 import { ReactComponent as TAG } from './svg/tagLogo.svg';
 import { ReactComponent as ThirdANDGrove } from './svg/thirdAndGroveLogo.svg';
+import { ReactComponent as Close } from './svg/close.svg';
+import { ReactComponent as Hamburger } from './svg/hamburger.svg';
 import { colors } from '../../styles';
 
 const TopNav = () => {
@@ -60,86 +62,7 @@ const TopNav = () => {
           type='button'
           onClick={() => toggleOpen()}
         >
-          {/* Hamburger / 'X' */}
-          {isOpen ? (
-            <svg
-              width='25px'
-              height='25px'
-              viewBox='0 0 21 21'
-              version='1.1'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <g
-                id='Concept-01'
-                stroke='none'
-                strokeWidth='1'
-                fill='none'
-                fillRule='evenodd'
-              >
-                <g
-                  id='Desktop---Menu'
-                  transform='translate(-1287.000000, -33.000000)'
-                >
-                  <rect x='0' y='0' width='1440' height='700' />
-                  <g
-                    id='X-Icon'
-                    transform='translate(1287.000000, 33.000000)'
-                    fill='#282829'
-                  >
-                    <rect
-                      id='Rectangle'
-                      transform='translate(10.500000, 10.500000) rotate(45.000000) translate(-10.500000, -10.500000) '
-                      x='-2'
-                      y='9'
-                      width='25'
-                      height='3'
-                    />
-                    <rect
-                      id='Rectangle'
-                      transform='translate(10.500000, 10.500000) rotate(-45.000000) translate(-10.500000, -10.500000) '
-                      x='-2'
-                      y='9'
-                      width='25'
-                      height='3'
-                    />
-                  </g>
-                </g>
-              </g>
-            </svg>
-          ) : (
-            <svg
-              width='25px'
-              height='25px'
-              viewBox='0 0 25 23'
-              version='1.1'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <g
-                id='Concept-01'
-                stroke='none'
-                strokeWidth='1'
-                fill='none'
-                fillRule='evenodd'
-              >
-                <g
-                  id='Desktop---Work'
-                  transform='translate(-1285.000000, -32.000000)'
-                >
-                  <rect x='0' y='0' width='1440' height='5040' />
-                  <g id='Hero' fill='#282829'>
-                    <g
-                      id='Hamburger'
-                      transform='translate(1285.000000, 32.000000)'
-                    >
-                      <rect id='Rectangle' x='0' y='0' width='25' height='3' />
-                      <rect id='Rectangle' x='0' y='20' width='25' height='3' />
-                      <rect id='Rectangle' x='8' y='10' width='17' height='3' />
-                    </g>
-                  </g>
-                </g>
-              </g>
-            </svg>
-          )}
+          {isOpen ? <Hamburger /> : <Close />}
         </button>
       </span>
       {/* // some transition in/out? */}
