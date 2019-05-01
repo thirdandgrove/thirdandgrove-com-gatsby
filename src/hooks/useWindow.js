92c2094b482e4ed3a0c22be30389fe96e1513b2b
Grant Glidewell <grant@thirdandgrove.com>
2019-05-01 09:59:48 -0700

    further protections added in hook for build

--- src/hooks/useWindow.js
+++ src/hooks/useWindow.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 export default () => {
-  const [windowSize, setWindowSize] = useState(() => ({
-    width: window && window.innerWidth,
-    height: window && window.innerHeight,
-  }));
+  const [windowSize, setWindowSize] = useState(() => {
+    return (
+      typeof window !== 'undefined' && {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }
+    );
+  });
 
   useEffect(() => {
     const onResize = () =>
+      typeof window !== 'undefined' &&
       setWindowSize({
-        width: window && window.innerWidth,
-        height: window && window.innerHeight,
+        width: window.innerWidth,
+        height: window.innerHeight,
       });
-    if (window) window.addEventListener('resize', onResize);
-    return () => window && window.removeEventListener('resize', onResize);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', onResize);
+    }
+    return () =>
+      typeof window !== 'undefined' &&
+      window.removeEventListener('resize', onResize);
   });
 
   return windowSize;
