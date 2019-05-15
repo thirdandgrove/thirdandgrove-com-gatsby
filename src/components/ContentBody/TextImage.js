ffd1e0efe745473959371b0c6f98bd440eab6a38
Grant Glidewell <grant@thirdandgrove.com>
2019-05-15 07:26:19 -0700

    added: split components

--- src/components/ContentBody/TextImage.js
+++ src/components/ContentBody/TextImage.js
@@ -1,3 +1,15 @@
 import React from 'react';
 
-export default ({ data }) => <div>{JSON.stringify(data)}</div>;
+import SplitSection from '../SplitSection';
+
+export default ({ data }) => {
+  return (
+    <SplitSection padding='1rem'>
+      <span dangerouslySetInnerHTML={{ __html: data.field_body.value }} />
+      <img
+        src={data.relationships.field_image.localFile.publicURL}
+        alt='article text split'
+      />
+    </SplitSection>
+  );
+};
--- src/components/ContentBody/TextQuote.js
+++ src/components/ContentBody/TextQuote.js
@@ -1,3 +1,41 @@
 import React from 'react';
+import { css } from '@emotion/core';
 
-export default ({ data }) => <div>{JSON.stringify(data)}</div>;
+import SplitSection from '../SplitSection';
+
+export default ({ data }) => {
+  return (
+    <SplitSection padding='1rem'>
+      {data.field_reversed ? (
+        <>
+          {' '}
+          <span
+            dangerouslySetInnerHTML={{ __html: data.field_body.processed }}
+          />
+          <p
+            css={css`
+              margin: 0 auto;
+              align-self: center;
+            `}
+          >
+            {data.field_quote}
+          </p>
+        </>
+      ) : (
+        <>
+          <p
+            css={css`
+              align-self: center;
+              margin: 0 auto;
+            `}
+          >
+            {data.field_quote}
+          </p>
+          <span
+            dangerouslySetInnerHTML={{ __html: data.field_body.processed }}
+          />
+        </>
+      )}
+    </SplitSection>
+  );
+};
