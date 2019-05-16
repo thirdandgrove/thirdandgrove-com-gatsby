e6ac05dadcfba274c06d61f67a66fec5fba8625f
Grant Glidewell <grant@thirdandgrove.com>
2019-05-16 15:48:48 -0700

    careers filter styled, not functional

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,11 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
 import Layout from '../components/layout';
+import FullWidthSection from '../components/FullWidthSection';
 
 export default () => {
+  const Filter = styled.nav`
+    width: 100%;
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      width: 50%;
+      margin: 5rem auto;
+      margin-top: 1rem;
+    }
+    li {
+      text-decoration: none;
+      font-variant: small-caps;
+      list-style: none;
+      font-family: NBInternationalPro-Bol;
+      font-size: 15px;
+      color: #757575;
+      letter-spacing: 2px;
+      text-align: right;
+      line-height: 36px;
+      padding-right: 15px;
+      cursor: pointer;
+      &.active {
+        color: #282829;
+        text-decoration: underline;
+      }
+    }
+  `;
   return (
-    <Layout>
-      <p>careers</p>
+    <Layout
+      headerData={{
+        title: 'Careers',
+        height: '450px',
+      }}
+    >
+      <Filter>
+        <ul
+          css={css`
+            margin: 0 auto !important;
+          `}
+        >
+          <li
+            css={css`
+              margin: 0 auto;
+            `}
+          >
+            filter jobs
+          </li>
+        </ul>
+        <ul>
+          <li>creative</li>
+          <li>delivery</li>
+          <li>strategy</li>
+          <li className='active'>engineering</li>
+        </ul>
+      </Filter>
+      <FullWidthSection>
+        <p>joblist</p>
+      </FullWidthSection>
     </Layout>
   );
 };
+
+// TODO: fetch data from Jazz API
