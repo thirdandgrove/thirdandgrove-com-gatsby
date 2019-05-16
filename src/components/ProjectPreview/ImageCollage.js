18876f1282cfb8a31a59eda569fa97aa84038283
Grant Glidewell <grant@thirdandgrove.com>
2019-05-16 12:52:03 -0700

    mode concise dynamic selection

--- src/components/ProjectPreview/ImageCollage.js
+++ src/components/ProjectPreview/ImageCollage.js
@@ -3,33 +3,77 @@ import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
 const ImageCollage = ({ index, images }) => {
+  const slideNumber = index + 1;
   const gridWrap = css`
-    position: relative;
-    left: -30vw;
     z-index: 1;
     display: grid;
-    grid-template-rows: repeat(3, 1rem);
-    grid-template-columns: repeat(3, 1rem);
-  `;
-  const collage1 = css`
-    .primary {
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+
+    position: relative;
+    height: 500px;
+    width: 50vw;
+    left: -30vw;
+
+    .primary1 {
       background-color: yellow;
       grid-area: 1 / 1;
     }
-    .secondary {
+    .primary2 {
+      background-color: yellow;
+      grid-area: 1 / 1;
+    }
+    .primary3 {
+      background-color: yellow;
+      grid-area: 1 / 1;
+    }
+
+    .secondary1 {
       background-color: blue;
       grid-area: 2 / 3;
     }
-    .tertiary {
+    .secondary2 {
+      background-color: blue;
+      grid-area: 2 / 3;
+    }
+    .secondary3 {
+      background-color: blue;
+      grid-area: 2 / 3;
+    }
+
+    .tertiary1 {
+      background-color: orange;
+      grid-area: 3 / 1;
+    }
+    .tertiary2 {
+      background-color: orange;
+      grid-area: 3 / 1;
+    }
+    .tertiary3 {
       background-color: orange;
       grid-area: 3 / 1;
     }
   `;
+  const selectedClass = () =>
+    // eslint-disable-next-line no-nested-ternary
+    slideNumber % 3 === 0 ? 3 : slideNumber % 2 === 0 ? 2 : 1;
   return (
-    <article css={[gridWrap, collage1]}>
-      <img src={images.primary} alt='primary' className='primary' />
-      <img src={images.secondary} alt='secondary' className='secondary' />
-      <img src={images.tertiary} alt='tertiary' className='tertiary' />
+    <article css={gridWrap}>
+      <img
+        src={images.primary}
+        alt='primary'
+        className={`primary${selectedClass()}`}
+      />
+      <img
+        src={images.secondary}
+        alt='secondary'
+        className={`secondary${selectedClass()}`}
+      />
+      <img
+        src={images.tertiary}
+        alt='tertiary'
+        className={`tertiary${selectedClass()}`}
+      />
     </article>
   );
 };
