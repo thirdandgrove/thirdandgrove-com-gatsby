5d6328738057428944514f41d8b19097b97ff20b
Grant Glidewell <grant@thirdandgrove.com>
2019-05-29 10:47:19 -0700

    added: list of interests, link to job postings page

--- src/components/WhereWeAre/WhereWeAre.js
+++ src/components/WhereWeAre/WhereWeAre.js
@@ -1,16 +1,25 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
+import { navigate } from 'gatsby';
 import { css } from '@emotion/core';
 import Slider from 'react-slick';
 
 import FullWidthSection from '../FullWidthSection';
 import { colors } from '../../styles';
+import Button from '../Button';
 
 export default ({ h3style, h4style, h1style }) => {
   const [selected, updateSelection] = useState('boston');
+  const listCss = css`
+    list-style: none;
+    li {
+      ${h1style}
+      padding: 2rem 0;
+    }
+  `;
   return (
-    <FullWidthSection>
+    <FullWidthSection height='100%'>
       <h3 css={h3style}>Where We Are</h3>
       <h1 css={h1style}>Offices if we need them.</h1>
       <section
@@ -94,7 +103,65 @@ export default ({ h3style, h4style, h1style }) => {
           {selected === 'oakland' && <h4>...copy for oakland</h4>}
         </article>
       </section>
-      <Slider>{/* content pending */}</Slider>
+      <Slider>{/* image content pending */}</Slider>
+      {selected === 'boston' && (
+        <>
+          <h3 css={h3style}>The good shit in Boston</h3>
+          <ul css={listCss}>
+            <li>Tanám</li>
+            <li>Night Shift Brewing</li>
+            <li>Alcove</li>
+            <li>Kamakura</li>
+            <li>Fox &amp; the knife</li>
+            <li>Night Shift Brewing</li>
+          </ul>
+        </>
+      )}
+      {selected === 'oakland' && (
+        <>
+          <h3 css={h3style}>The good shit in Oakland</h3>
+          <ul css={listCss}>
+            <li>Other</li>
+            <li>Places</li>
+            <li>Oaktown</li>
+            <li>IS Known For</li>
+          </ul>
+        </>
+      )}
+      <FullWidthSection
+        height='550px'
+        css={css`
+          background-color: ${colors.lightblue};
+          z-index: 1;
+          height: 550px;
+          &::after {
+            content: '';
+            background: url('images/menuBackdrop.webp');
+            opacity: 0.15;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            position: absolute;
+            z-index: -1;
+          }
+        `}
+      >
+        <h3 css={h3style}>Making Moves?</h3>
+        <h1
+          css={[
+            h1style,
+            css`
+              padding: 2rem 0;
+            `,
+          ]}
+        >
+          Show us what you&apos;re made of.
+        </h1>
+        <Button onClick={() => navigate(`/careers`)}>
+          view open positions
+        </Button>
+      </FullWidthSection>
     </FullWidthSection>
   );
 };
