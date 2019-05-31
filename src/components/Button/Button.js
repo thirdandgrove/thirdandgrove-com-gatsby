60942392bb9518cbab3313d7610192e06757f9e3
Abby Milberg <abby.milberg@bixal.com>
2019-05-30 20:03:36 -0400

    Starting work on button hover animation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,19 +3,39 @@ import styled from '@emotion/styled';
 import { colors } from '../../styles';
 
 export default styled.button`
-  background-color: transparent;
-  outline: none;
+  position: relative;
+  min-width: 220px;
+  padding: 11px 15px;
   border: none;
-  border-bottom: 1px solid ${colors.darkgray};
-  width: 220px;
-  height: 18px;
-  color: ${colors.darkgray};
+  outline: none;
   font-family: 'NB International Pro';
   font-weight: 600;
   font-size: 15px;
+  line-height: 1.2;
   letter-spacing: 2px;
-  line-height: 36px;
-  padding-bottom: 40px;
   text-transform: uppercase;
+  background: transparent;
+  color: ${colors.darkgray};
   cursor: pointer;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    transition: 0.3s ease height;
+    background: ${colors.darkgray};
+  }
+
+  &:hover,
+  &:focus {
+    color: ${colors.white};
+
+    &::before {
+      height: 100%;
+    }
+  }
 `;
