6b4bed8b562841be79ade5742a4bab4d559a7b16
Grant Glidewell <grant@thirdandgrove.com>
2019-06-03 09:37:20 -0700

    scafolded work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,11 +1,105 @@
 import React from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 
 import Layout from '../components/layout';
+import { colors } from '../styles';
+import FullWidthSection from '../components/FullWidthSection';
 
 export default () => {
+  const Categories = styled.nav`
+    width: 100%;
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      width: 50%;
+      margin: 5rem auto;
+    }
+    li {
+      text-decoration: none;
+      font-variant: small-caps;
+      list-style: none;
+      font-family: NBInternationalPro-Bol;
+      font-size: 15px;
+      color: #757575;
+      letter-spacing: 2px;
+      text-align: right;
+      line-height: 36px;
+      padding-right: 15px;
+      cursor: pointer;
+      &.active {
+        color: ${colors.darkgray};
+      }
+    }
+  `;
+  const StudyPreview = styled.div``;
   return (
-    <Layout>
-      <p>work</p>
-    </Layout>
+    <StaticQuery
+      query={graphql`
+        {
+          allCaseStudy {
+            nodes {
+              id
+              title
+              field_subtitle
+              field_inverse_header
+              field_primary_image_scale
+              field_tertiary_image_scale
+              field_secondary_image_scale
+              relationships {
+                field_tags {
+                  name
+                }
+                field_image {
+                  id
+                  localFile {
+                    publicURL
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const studies = data.allCaseStudy.nodes;
+        return (
+          <Layout
+            headerData={{
+              children: (
+                <>
+                  <p>google</p>
+                  <p>google</p>
+                  <p>google</p>
+                </>
+              ),
+              title: 'We work with brands we love.',
+            }}
+          >
+            <Categories>
+              {/* query generated from tags? */}
+              <ul>
+                <li className='active'>all</li>
+                <li>design</li>
+                <li>strategy</li>
+                <li>engineering</li>
+                <li>shopify</li>
+                <li>acquia</li>
+                <li>drupal</li>
+              </ul>
+            </Categories>
+            <FullWidthSection>
+              {studies.map(study => (
+                <StudyPreview>
+                  <pre>{JSON.stringify(study)}</pre>
+                </StudyPreview>
+              ))}
+            </FullWidthSection>
+          </Layout>
+        );
+      }}
+    />
   );
 };
