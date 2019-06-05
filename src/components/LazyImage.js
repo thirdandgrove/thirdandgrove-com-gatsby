fd96a674629ae4c260b54e57fe74efc24a9ced38
Grant Glidewell <grant@thirdandgrove.com>
2019-06-05 10:44:53 -0700

    refactored to useStaticQuery

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 /*
@@ -13,34 +13,32 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
-const Image = ({ id }) => (
-  // This is heavy, though a real dynamic query by ID is not possible
-  // currently this will remain in use until an alternative is made available
-  <StaticQuery
-    query={graphql`
-      query {
-        allFileFile {
-          nodes {
-            id
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 1800) {
-                  ...GatsbyImageSharpFluid
-                }
+const Image = ({ id }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFileFile {
+        nodes {
+          id
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1800) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
         }
       }
-    `}
-    render={data => (
-      <Img
-        fluid={
-          data.allFileFile.nodes.find(node => node.id === id).localFile
-            .childImageSharp.fluid
-        }
-      />
-    )}
-  />
-);
+    }
+  `);
+  // This is heavy, though a real dynamic query by ID is not possible
+  // currently this will remain in use until an alternative is made available
+  return (
+    <Img
+      fluid={
+        data.allFileFile.nodes.find(node => node.id === id).localFile
+          .childImageSharp.fluid
+      }
+    />
+  );
+};
 export default Image;
