c00a76a0b03c31b541594dee085b83ce03fca3fe
Grant Glidewell <grant@thirdandgrove.com>
2019-06-12 06:00:27 -0700

    removed superfluous static query from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Global, css } from '@emotion/core';
+import { Global } from '@emotion/core';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -9,25 +8,12 @@ import '../styles/layout.css';
 import { globalStyles } from '../styles';
 
 const Layout = ({ children, headerData }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={() => (
-      <>
-        <Global styles={globalStyles} />
-        <Header {...headerData} />
-        <main>{children}</main>
-        <Footer />
-      </>
-    )}
-  />
+  <>
+    <Global styles={globalStyles} />
+    <Header {...headerData} />
+    <main>{children}</main>
+    <Footer />
+  </>
 );
 
 Layout.propTypes = {
