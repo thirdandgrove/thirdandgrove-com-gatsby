77f1694ff89751dff04f3128c3a00c95c1fd5138
Abby Milberg <abby.milberg@bixal.com>
2019-06-19 18:15:05 -0400

    Typography for case study pre-footer.

--- src/components/ContentBody/Prefooter.js
+++ src/components/ContentBody/Prefooter.js
@@ -3,41 +3,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import Img from 'gatsby-image';
-
+import Button from '../Button';
 import { TextWrapper } from '../Prefooter';
 import SplitSection from '../SplitSection';
-import { colors, fonts, weights } from '../../styles';
+import { colors, fonts, weights, mediaQueries } from '../../styles';
+
+const pStyles = css`
+  margin-bottom: 25px;
+  font-size: 24px;
+  font-family: ${fonts.serif};
+  font-weight: ${weights.thin};
+  ${mediaQueries.phoneLarge} {
+    margin-bottom: 0;
+  }
+`;
+
+const h2Styles = css`
+  margin-bottom: 35px;
+  font-size: 45px;
+  line-height: 1.7;
+  letter-spacing: 0.6px;
+  font-weight: ${weights.black};
+  ${mediaQueries.phoneLarge} {
+    margin-bottom: 0;
+    font-size: 72px;
+    line-height: 1;
+    letter-spacing: 0.96px;
+  }
+`;
+
+const wrapperStyles = css`
+   {
+    /* TODO: change data model to provide a color. */
+  }
+  background-color: ${colors.yellow};
+  min-height: 300px;
+  padding: 40px 20px;
+  text-align: center;
+
+  ${mediaQueries.phoneLarge} {
+    min-height: 610px;
+    padding-top: 0;
+    padding-bottom: 50px;
+    justify-content: space-evenly;
+  }
+`;
 
 const Prefooter = ({ data }) => (
   <SplitSection>
-    {/* TODO: change data model to provide a color */}
-    <TextWrapper backgroundColor={colors.yellow}>
-      {/* TODO: change data model to reference another article? */}
-      <span
-        css={css`
-          a {
-            background-color: transparent;
-            outline: none;
-            border: none;
-            border-bottom: 1px solid ${colors.darkgray};
-            width: 220px;
-            height: 18px;
-            color: ${colors.darkgray};
-            font-family: ${fonts.sans};
-            font-weight: ${weights.bold};
-            font-size: 15px;
-            letter-spacing: 2px;
-            line-height: 36px;
-            padding-bottom: 14px;
-            text-transform: uppercase;
-            text-decoration: none;
-            cursor: pointer;
-          }
-        `}
-        dangerouslySetInnerHTML={{
-          __html: data.field_secondary_body.processed,
-        }}
-      />
+    <TextWrapper css={wrapperStyles}>
+      {/* TODO: change data model to reference another article. */}
+      <p css={pStyles}>Up Next</p>
+      <h2 css={h2Styles}>Another Client</h2>
+      <Button>View Case Study</Button>
     </TextWrapper>
     <Img
       fluid={data.relationships.field_image.localFile.childImageSharp.fluid}
