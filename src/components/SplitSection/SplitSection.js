5a48f1a4e018e2d90202397774543d219a2ebcb8
Abby Milberg <abby805@gmail.com>
2019-06-25 16:59:58 -0400

    Making the grid customizable on SplitSection

--- src/components/SplitSection/SplitSection.js
+++ src/components/SplitSection/SplitSection.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
 import { mediaQueries } from '../../styles';
 
-const Wrapper = styled.div`
-  ${mediaQueries.phoneLarge} {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-  }
-`;
-const SplitSection = ({ children, ...props }) => (
-  <Wrapper {...props}>{children}</Wrapper>
+const SplitSection = ({ children, gridOverride, ...props }) => (
+  <div
+    {...props}
+    css={css`
+      ${mediaQueries.phoneLarge} {
+        display: grid;
+        grid-template-columns: ${gridOverride};
+        justify-content: space-between;
+      }
+    `}
+  >
+    {children}
+  </div>
 );
 
 SplitSection.propTypes = {
+  gridOverride: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
+};
+
+SplitSection.defaultProps = {
+  gridOverride: 'repeat(2, 1fr)',
 };
 
 export default SplitSection;
