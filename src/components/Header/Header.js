643f50bd2e5a2c159116ed3ad5af6e4bb368842f
Claire Annan <cannan@thirdandgrove.com>
2019-07-03 14:30:21 -0400

    Blog detail header label settings.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,14 @@ import { css } from '@emotion/core';
 
 import TopNav from '../TopNav';
 import SEO from '../seo';
-import { colors, mediaQueries, weights } from '../../styles';
+import { colors, fonts, mediaQueries, weights } from '../../styles';
 import FullWidthSection from '../FullWidthSection';
 
 const Header = ({
   defaultBackground,
   backgroundImage,
   title,
+  label,
   metaTitle,
   description,
   fade,
@@ -44,7 +45,7 @@ const Header = ({
     letter-spacing: -0.45px;
     width: 60%;
     text-align: center;
-    color: ${defaultBackground ? colors.darkgray : '#efefef'};
+    color: ${defaultBackground ? colors.darkgray : color.lightgray};
 
     ${mediaQueries.phoneLarge} {
       font-size: 72px;
@@ -57,6 +58,22 @@ const Header = ({
     background-image: url(${backgroundImage});
     background-color: ${color};
   `;
+  const headerlabel = css`
+    margin-top: 68px;
+    margin-bottom: 40px;
+    font-family: ${fonts.sans};
+    font-size: 15px;
+    font-weight: ${weights.light};
+    letter-spacing: 2px;
+    line-height: 36px;
+    text-transform: capitalize;
+    color: ${colors.reallydarkgray};
+
+    ${mediaQueries.desktop} {
+      margin-top: -100px;
+      margin-bottom: 60px;
+    }
+  `;
   return (
     <>
       <SEO title={title || metaTitle} description={description} />
@@ -66,6 +83,11 @@ const Header = ({
         height={height}
         mobileHeight={mobileHeight}
       >
+        {label && (
+          <span data-cy='labelText' css={headerlabel}>
+            {label}
+          </span>
+        )}
         {title && (
           <h1 data-cy='titleText' css={headerTitle}>
             {title}
@@ -80,6 +102,7 @@ const Header = ({
 Header.propTypes = {
   defaultBackground: PropTypes.bool,
   title: PropTypes.string,
+  label: PropTypes.string,
   metaTitle: PropTypes.string,
   description: PropTypes.string,
   fade: PropTypes.number,
@@ -94,6 +117,7 @@ Header.propTypes = {
 Header.defaultProps = {
   defaultBackground: true,
   title: null,
+  label: null,
   metaTitle: null,
   description: null,
   fade: 0,
