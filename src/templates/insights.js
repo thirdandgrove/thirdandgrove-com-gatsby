c6c9ed8f3db00a5a00cd67313fbd8eab254a4eae
Claire Annan <cannan@thirdandgrove.com>
2019-07-08 08:31:29 -0400

    Remove unused fonts and weights imports.

--- src/templates/insights.js
+++ src/templates/insights.js
@@ -4,7 +4,7 @@ import { css } from '@emotion/core';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
-import { colors, fonts, mediaQueries, weights } from '../styles';
+import { colors, mediaQueries } from '../styles';
 import Layout from '../components/layout';
 import ContentBody from '../components/ContentBody';
 
