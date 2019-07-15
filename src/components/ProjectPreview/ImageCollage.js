803722d98edf74a7873e0c66c6f31bfd694cee38
Abby Milberg <abby805@gmail.com>
2019-07-15 10:45:00 -0400

    Tweaking image alignment on one of the homepage project slide styles.

--- src/components/ProjectPreview/ImageCollage.js
+++ src/components/ProjectPreview/ImageCollage.js
@@ -24,30 +24,30 @@ const ImageCollage = ({ images, type }) => {
 
       &.type-a {
         left: 430px;
-        right: 70px;
+        right: 60px;
 
         .collage-board {
-          padding-top: 75%;
+          padding-top: 74%;
         }
 
         .primary-wrapper {
           top: 0;
           left: 0;
-          width: 62.5%;
+          width: 61.6%;
           height: 59.26%;
         }
 
         .secondary-wrapper {
-          bottom: 0;
-          right: 37.5%;
-          width: 34.72%;
+          bottom: 2%;
+          right: 38.4%;
+          width: 34.25%;
           height: 33.33%;
         }
 
         .tertiary-wrapper {
           bottom: 0;
           right: 0;
-          width: 34.72%;
+          width: 34.25%;
           height: 91.67%;
         }
       }
