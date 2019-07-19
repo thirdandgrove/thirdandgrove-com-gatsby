bbeaf3b69526d3c826bf97a38f576d41d098e2ce
Abby Milberg <abby805@gmail.com>
2019-07-19 17:36:17 -0400

    Working on video player

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,15 +1,11 @@
+/* eslint-disable prefer-template */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
 import ReactPlayer from 'react-player';
 
 import useWindow from '../../hooks/useWindow';
-import {
-  colors,
-  mediaQueries,
-  jsBreakpoints,
-  smSectionHead,
-  container,
-} from '../../styles';
+import { mediaQueries, jsBreakpoints } from '../../styles';
 import FullWidthSection from '../FullWidthSection';
 
 const VideoSection = ({ url }) => {
@@ -17,25 +13,82 @@ const VideoSection = ({ url }) => {
   const isSmScreen = width < jsBreakpoints.phoneLarge;
   const [playing, setPlaying] = useState(!isSmScreen);
   const [hasInteracted, setHasInteracted] = useState(false);
+  const [theUrl, setUrl] = useState(url);
+
+  const sectionStyles = css`
+    position: relative;
+  `;
+
+  const playerStyles = css`
+    position: relative;
+    height: 0 !important;
+    padding-top: 93vh;
+    overflow: hidden;
+
+    > div {
+      // Don't get me started on this thing's markup
+      position: absolute;
+      top: 0;
+
+      > div {
+        padding-top: 93vh !important;
+      }
+    }
+
+    iframe {
+      width: 165.4vh !important;
+      left: 50% !important;
+      margin: 0 0 0 -82.7vh;
+    }
+
+    ${mediaQueries.phoneLarge} {
+      padding-top: 56.25%;
+
+      > div > div {
+        padding-top: 56.25% !important;
+      }
+
+      iframe {
+        width: 100% !important;
+        left: 0 !important;
+        margin: 0;
+      }
+    }
+  `;
+
+  const btnStyles = css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  `;
 
   return (
-    <FullWidthSection>
+    <FullWidthSection height='0' mobileHeight='0' css={sectionStyles}>
       <button
         type='button'
+        css={btnStyles}
         onClick={() => {
-          setPlaying(!playing);
           if (!hasInteracted) {
             setHasInteracted(true);
+            // Starting the video over from the beginning now that the user
+            // has chosen to watch and it's unmuted. Adding the ? because if
+            // I reset it to the exact same URL, Gatsby is smart/dumb enough to
+            // realize it's the same and just continue on.
+            setUrl(url + '?');
+          } else {
+            setPlaying(!playing);
           }
         }}
       >
-        Play/Pause
+        {hasInteracted && playing ? 'Pause' : 'Play'}
       </button>
       <ReactPlayer
         // see: https://www.npmjs.com/package/react-player for props
         width={width}
-        height='800px'
-        url={url}
+        css={playerStyles}
+        url={theUrl}
         playing={playing}
         volume={hasInteracted ? 1 : 0} // Mute on autoplay
         config={{
