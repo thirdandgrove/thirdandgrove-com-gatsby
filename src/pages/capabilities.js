4a0f26c786b1af14be2e886dda4dabc00a79161a
Wes Jones <waj194@gmail.com>
2019-07-19 17:58:58 -0400

    T5-52 Style Capabilities page, using similar structure to the Work page, and still use a placeholder element for the image.

--- src/pages/capabilities.js
+++ src/pages/capabilities.js
@@ -1,119 +1,204 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Spring } from 'react-spring/renderprops';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
-import { colors, fonts, weights } from '../styles';
+import { colors, fonts, mediaQueries, container, weights } from '../styles';
 import Layout from '../components/layout';
-import SplitSection from '../components/SplitSection';
+import FullWidthSection from '../components/FullWidthSection';
+import { useHasBeenVisible } from '../hooks/useVisibility';
+
+const Capability = ({ content, index }) => {
+  const nodeRef = useRef();
+  const isVisible = useHasBeenVisible(nodeRef);
+
+  const Imageplaceholder = styled.div`
+    height: 333px;
+    width: 333px;
+    background-color: ${colors.gray};
+
+    ${mediaQueries.phoneLarge} {
+      height: 475px;
+      width: 475px;
+    }
+  `;
+
+  return (
+    <FullWidthSection
+      ref={nodeRef}
+      height='0'
+      padding='0'
+      textAlign='left'
+      css={css`
+        &:first-of-type {
+          margin-top: 20px;
+
+          ${mediaQueries.phoneLarge} {
+            margin-top: 175px;
+          }
+        }
+      `}
+    >
+      <div css={container.medium}>
+        <div
+          css={css`
+            display: block;
+            margin-bottom: 125px;
+
+            ${mediaQueries.phoneLarge} {
+              display: flex;
+              justify-content: space-between;
+              flex-direction: ${index % 2 ? 'row-reverse' : 'row'};
+              align-items: center;
+              margin-bottom: 170px;
+            }
+
+            ul {
+              margin: 0;
+              li {
+                font-family: ${fonts.sans};
+                font-weight: ${weights.bold};
+                font-variant-caps: all-small-caps;
+                letter-spacing: 1px;
+                list-style: none;
+              }
+            }
+          `}
+        >
+          <Spring
+            delay={0}
+            to={{
+              transform: isVisible ? 'translateY(0)' : 'translateY(200px)',
+              opacity: isVisible ? '1' : '0',
+            }}
+          >
+            {({ transform, opacity }) => (
+              <Imageplaceholder
+                style={{ transform, opacity }}
+                css={css`
+                  width: 100%;
+                  margin-bottom: 20px;
+
+                  ${mediaQueries.phoneLarge} {
+                    flex-grow: 0;
+                    flex-shrink: 0;
+                    flex-basis: ${index % 2 ? '64%' : '49%'};
+                    width: ${index % 2 ? '64%' : '49%'};
+                    /* The following line is strictly for placeholder purposes. */
+                    height: ${index % 4 === 2 ? '630px' : 'auto'};
+                    margin-bottom: 0;
+
+                    > div {
+                      padding-bottom: ${index % 2 ? '76% !important' : '100%'};
+                      padding-bottom: ${index % 4 === 2
+                        ? '131% !important'
+                        : '100%'};
+                    }
+                  }
+                `}
+              >
+                <div />
+              </Imageplaceholder>
+            )}
+          </Spring>
+
+          <div
+            css={css`
+              position: relative;
+
+              ${mediaQueries.phoneLarge} {
+                flex-grow: 0;
+                flex-shrink: 0;
+                flex-basis: ${index % 2 ? '30%' : '40%'};
+                width: ${index % 2 ? '30%' : '40%'};
+              }
+            `}
+          >
+            {content}
+          </div>
+        </div>
+      </div>
+    </FullWidthSection>
+  );
+};
+
+Capability.propTypes = {
+  content: PropTypes.node.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default () => {
-  const Block = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 450px;
-    padding: 3rem;
-    h2 {
-      font-family: ${fonts.serif};
-    }
-    p {
-      font-family: ${fonts.serif};
-      font-weight: ${weights.regular};
-    }
-    ul {
-      margin: 0;
-      li {
-        font-family: ${fonts.sans};
-        font-weight: ${weights.bold};
-        font-variant-caps: all-small-caps;
-        letter-spacing: 1px;
-        list-style: none;
-      }
-    }
-  `;
-  const Imageplaceholder = styled.div`
-    height: 500px;
-    width: 500px;
-    background-color: ${colors.gray};
-  `;
   return (
     <Layout
-      headerData={{ title: `All the stuff we're good at`, height: `400px` }}
+      headerData={{
+        title: `All the stuff we're good at.`,
+        mobileHeight: '93vh',
+        height: '400px',
+      }}
     >
-      <SplitSection
-        css={css`
-          justify-items: center;
-          place-items: center;
-          padding: 6rem;
-        `}
-      >
-        <Imageplaceholder />
-        <Block>
-          <h2>Technology</h2>
-          <p>
-            Work with the best engineers in the room-no matter what room
-            you&quot;re in. Our engineers are writing and discovering the future
-            of digital excellence
-          </p>
-          <ul>
-            <li>Front-End Development</li>
-            <li>Back-End Development</li>
-            <li>Content Management</li>
-            <li>Drupal</li>
-            <li>Gatsby</li>
-            <li>Shopify</li>
-          </ul>
-        </Block>
-      </SplitSection>
-      <SplitSection
-        css={css`
-          justify-items: center;
-          place-items: center;
-          padding: 6rem;
-        `}
-      >
-        <Block>
-          <h2>Strategy</h2>
-          <p>
-            The foundation for great work. Know your customer, your goals and
-            how to reach them.
-          </p>
-          <ul>
-            <li>Digital Strategy</li>
-            <li>Trends &amp; Insights</li>
-            <li>Customer Research</li>
-            <li>Industry Research</li>
-            <li>Analytics / Data / SEO</li>
-            <li>Omnichannel Strategy</li>
-          </ul>
-        </Block>
-        <Imageplaceholder />
-      </SplitSection>
-      <SplitSection
-        css={css`
-          justify-items: center;
-          place-items: center;
-          padding: 6rem;
-        `}
-      >
-        <Imageplaceholder />
-        <Block>
-          <h2>Creative</h2>
-          <p>
-            Where data, culture, and good looks come together. Create the
-            strongest connection to the brand experience, and look good doing
-            it.
-          </p>
-          <ul>
-            <li>Art Direction</li>
-            <li>UI/UX Design</li>
-            <li>Responsive Design</li>
-            <li>Social Content</li>
-            <li>Content Development</li>
-            <li>Interaction / Motion</li>
-          </ul>
-        </Block>
-      </SplitSection>
+      <Capability
+        content={
+          <>
+            <h2>Technology</h2>
+            <p>
+              Work with the best engineers in the room—no matter what room
+              you&quot;re in. Our engineers are writing and discovering the
+              future of digital excellence
+            </p>
+            <ul>
+              <li>Front-End Development</li>
+              <li>Back-End Development</li>
+              <li>Content Management</li>
+              <li>Drupal</li>
+              <li>Gatsby</li>
+              <li>Shopify</li>
+            </ul>
+          </>
+        }
+        index={0}
+      />
+      <Capability
+        content={
+          <>
+            <h2>Strategy</h2>
+            <p>
+              The foundation for great work. Know your customer, your goals and
+              how to reach them.
+            </p>
+            <ul>
+              <li>Digital Strategy</li>
+              <li>Trends &amp; Insights</li>
+              <li>Customer Research</li>
+              <li>Industry Research</li>
+              <li>Analytics / Data / SEO</li>
+              <li>Omnichannel Strategy</li>
+            </ul>
+          </>
+        }
+        index={1}
+      />
+      <Capability
+        content={
+          <>
+            <h2>Creative</h2>
+            <p>
+              Where data, culture, and good looks come together. Create the
+              strongest connection to the brand experience, and look good doing
+              it.
+            </p>
+            <ul>
+              <li>Art Direction</li>
+              <li>UI/UX Design</li>
+              <li>Responsive Design</li>
+              <li>Social Content</li>
+              <li>Content Development</li>
+              <li>Interaction / Motion</li>
+            </ul>
+          </>
+        }
+        index={2}
+      />
     </Layout>
   );
 };
