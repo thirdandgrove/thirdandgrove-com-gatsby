dacfc286d7d5bdbaf64bf06c146d0c6203d59fd9
Abby Milberg <abby805@gmail.com>
2019-07-19 15:26:15 -0400

    Starting on video theming

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -3,16 +3,33 @@ import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 
 import useWindow from '../../hooks/useWindow';
+import {
+  colors,
+  mediaQueries,
+  jsBreakpoints,
+  smSectionHead,
+  container,
+} from '../../styles';
 import FullWidthSection from '../FullWidthSection';
 
 const VideoSection = ({ url }) => {
   const { width } = useWindow();
-  const [playing, setPlaying] = useState(false);
+  const isSmScreen = width < jsBreakpoints.phoneLarge;
+  const [playing, setPlaying] = useState(!isSmScreen);
+  const [hasInteracted, setHasInteracted] = useState(false);
 
   return (
     <FullWidthSection>
-      <button type='button' onClick={() => setPlaying(!playing)}>
-        play/pause
+      <button
+        type='button'
+        onClick={() => {
+          setPlaying(!playing);
+          if (!hasInteracted) {
+            setHasInteracted(true);
+          }
+        }}
+      >
+        Play/Pause
       </button>
       <ReactPlayer
         // see: https://www.npmjs.com/package/react-player for props
@@ -20,12 +37,14 @@ const VideoSection = ({ url }) => {
         height='800px'
         url={url}
         playing={playing}
+        volume={hasInteracted ? 1 : 0} // Mute on autoplay
         config={{
           vimeo: {
             // see: https://developer.vimeo.com/api/oembed/videos for options
             playerOptions: {
               controls: false,
               responsive: true,
+              autoplay: !isSmScreen,
             },
           },
         }}
