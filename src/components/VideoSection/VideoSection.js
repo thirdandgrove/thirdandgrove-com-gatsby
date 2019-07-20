f215cedaa6e747f058621fd74c6822cbf6fc5467
Abby Milberg <abby805@gmail.com>
2019-07-20 17:51:45 -0400

    Almost done theming video, with exception of some performance issues.

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -5,7 +5,13 @@ import { css } from '@emotion/core';
 import ReactPlayer from 'react-player';
 
 import useWindow from '../../hooks/useWindow';
-import { mediaQueries, jsBreakpoints } from '../../styles';
+import {
+  mediaQueries,
+  jsBreakpoints,
+  colors,
+  fonts,
+  weights,
+} from '../../styles';
 import FullWidthSection from '../FullWidthSection';
 
 const VideoSection = ({ url }) => {
@@ -14,16 +20,21 @@ const VideoSection = ({ url }) => {
   const [playing, setPlaying] = useState(!isSmScreen);
   const [hasInteracted, setHasInteracted] = useState(false);
   const [theUrl, setUrl] = useState(url);
+  const [buttonX, setButtonX] = useState('50%');
+  const [buttonY, setButtonY] = useState('50%');
+  const [buttonVisible, setButtonVisible] = useState(false);
 
   const sectionStyles = css`
     position: relative;
+    background: ${colors.darkgray};
   `;
 
   const playerStyles = css`
     position: relative;
     height: 0 !important;
-    padding-top: 93vh;
+    padding-top: 550px;
     overflow: hidden;
+    opacity: ${hasInteracted && playing ? '1' : '0.65'};
 
     > div {
       // Don't get me started on this thing's markup
@@ -31,7 +42,7 @@ const VideoSection = ({ url }) => {
       top: 0;
 
       > div {
-        padding-top: 93vh !important;
+        padding-top: 550px !important;
       }
     }
 
@@ -57,33 +68,129 @@ const VideoSection = ({ url }) => {
   `;
 
   const btnStyles = css`
+    display: block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    border: solid 2px ${colors.white};
+    background: rgba(180, 180, 180, 0.2); // @todo update this
+    font-size: 0;
+    color: transparent;
+    cursor: pointer;
+
+    &:focus {
+      outline: none;
+    }
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      height: 0;
+      width: 0;
+      top: 30px;
+      left: 32px;
+      border: solid transparent 10px;
+      border-right-width: 0;
+      border-left: solid 16px ${colors.white};
+
+      ${mediaQueries.phoneLarge} {
+        display: none;
+      }
+    }
+
+    ${mediaQueries.phoneLarge} {
+      display: ${buttonVisible ? 'block' : 'none'};
+      position: fixed;
+      top: ${buttonY + 'px'};
+      left: ${buttonX + 'px'};
+      width: 102px;
+      height: 102px;
+      text-align: center;
+      font-size: 15px;
+      line-height: 6.8;
+      font-family: ${fonts.sans};
+      font-weight: ${weights.bold};
+      text-transform: uppercase;
+      color: ${colors.white};
+      transform: translate(-50%, -50%);
+      z-index: 3;
+      transition: 0.2s ease-out all;
+    }
+  `;
+
+  const h2Styles = css`
+    display: ${hasInteracted && playing ? 'none' : 'block'};
+    margin-left: 18px;
+    margin-bottom: 0;
+    font-size: 21px;
+    line-height: 2.3;
+    letter-spacing: -0.28px;
+    color: ${colors.white};
+
+    ${mediaQueries.phoneLarge} {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      font-size: 72px;
+      line-height: 1.16;
+      letter-spacing: -1px;
+    }
+  `;
+
+  const detailWrapper = css`
     position: absolute;
     top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1;
+    left: 20px;
+    right: 20px;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+
+    ${mediaQueries.phoneLarge} {
+      position: static;
+      transform: none;
+    }
   `;
 
   return (
-    <FullWidthSection height='0' mobileHeight='0' css={sectionStyles}>
-      <button
-        type='button'
-        css={btnStyles}
-        onClick={() => {
-          if (!hasInteracted) {
-            setHasInteracted(true);
-            // Starting the video over from the beginning now that the user
-            // has chosen to watch and it's unmuted. Adding the ? because if
-            // I reset it to the exact same URL, Gatsby is smart/dumb enough to
-            // realize it's the same and just continue on.
-            setUrl(url + '?');
-          } else {
-            setPlaying(!playing);
-          }
-        }}
-      >
-        {hasInteracted && playing ? 'Pause' : 'Play'}
-      </button>
+    <FullWidthSection
+      height='0'
+      mobileHeight='0'
+      css={sectionStyles}
+      onMouseEnter={() => setButtonVisible(true)}
+      onMouseLeave={() => setButtonVisible(false)}
+      onMouseMove={evt => {
+        setButtonX(evt.clientX);
+        setButtonY(evt.clientY);
+      }}
+    >
+      <div css={detailWrapper}>
+        <button
+          type='button'
+          css={btnStyles}
+          onClick={() => {
+            if (!hasInteracted) {
+              setHasInteracted(true);
+              // Starting the video over from the beginning now that the user
+              // has chosen to watch and it's unmuted. Adding the ? because if
+              // I reset it to the exact same URL, Gatsby is smart/dumb enough to
+              // realize it's the same and just continue on.
+              setUrl(url + '?');
+            } else {
+              setPlaying(!playing);
+            }
+          }}
+        >
+          {hasInteracted && playing ? 'Pause' : 'Play'}
+        </button>
+        <h2 css={h2Styles}>Watch Our Reel</h2>
+      </div>
       <ReactPlayer
         // see: https://www.npmjs.com/package/react-player for props
         width={width}
