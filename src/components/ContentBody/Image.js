fca5fb27272b2f41ab12a08795824baf559d1608
Claire Annan <cannan@thirdandgrove.com>
2019-07-22 13:43:12 -0400

    Update content Image Component styles.

--- src/components/ContentBody/Image.js
+++ src/components/ContentBody/Image.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import { css } from '@emotion/core';
+
+import { container, mediaQueries } from '../../styles';
 
 const Image = ({ data }) => (
-  <Img fluid={data.relationships.field_image.localFile.childImageSharp.fluid} />
+  <Img
+    fluid={data.relationships.field_image.localFile.childImageSharp.fluid}
+    css={css`
+      padding: 0 20px;
+
+      ${mediaQueries.desktop} {
+        ${container.min};
+        padding: 0;
+      }
+    `}
+  />
 );
 
 Image.propTypes = {
