99ef7d6ba76bed1d333e582c2b6879105ec8fb72
Claire Annan <cannan@thirdandgrove.com>
2019-08-02 10:54:03 -0400

    Apply new styles to drupal page.

--- src/pages/partners/drupal.js
+++ src/pages/partners/drupal.js
@@ -3,22 +3,47 @@ import React from 'react';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 
-import Layout from '../../components/layout';
-import FullWidthSection from '../../components/FullWidthSection';
-import ProjectsSlider from '../../components/ProjectsSlider';
-import LogoGrid from '../../components/LogoGrid';
-import { colors } from '../../styles';
-import SplitSection from '../../components/SplitSection';
-import InsightsSlider from '../../components/InsightsSlider';
-import Quote from '../../components/ContentBody/Quote';
+import Layout from '../components/layout';
+import FullWidthSection from '../components/FullWidthSection';
+import ProjectsSlider from '../components/ProjectsSlider';
+import LogoGrid from '../components/LogoGrid';
+import {
+  colors,
+  mediaQueries,
+  fonts,
+  weights,
+  container,
+  pLight,
+} from '../styles';
+import SplitSection from '../components/SplitSection';
+import InsightsSlider from '../components/InsightsSlider';
+import Quote from '../components/ContentBody/Quote';
 
 export default () => {
   const Tripple = styled.article`
-    justify-self: center;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-bottom: 80px;
+    h1 {
+      font-size: 48px;
+      margin-bottom: 8px;
+    }
+    p {
+      ${pLight};
+    }
+
+    ${mediaQueries.phoneLarge} {
+      justify-content: flex-start;
+    }
+  `;
+  const sectionPadding = css`
+    padding: 50px 20px;
+
+    ${mediaQueries.phoneLarge} {
+      padding: 110px 0;
+    }
   `;
   return (
     <Layout
@@ -28,19 +53,62 @@ export default () => {
           'Battle Tested Processes — Damn Good Engineers — Decoupled Pioneer',
         title: 'Drupal leader and contributor since inception.',
         color: colors.drupalBlue,
+        mobileMinHeight: '620px',
       }}
     >
-      <FullWidthSection height='400px' padding='0'>
-        <h4>A leader and contributor since inception</h4>
+      <FullWidthSection
+        height='400px'
+        align='left'
+        css={css`
+          ${sectionPadding}
+          h3 {
+            font-family: ${fonts.sans};
+            font-size: 18px;
+            font-weight: ${weights.bold};
+            line-height: 1.39;
+            margin-bottom: 12px;
+          }
+          p {
+            font-weight: ${weights.light};
+          }
+          div {
+            ${mediaQueries.phoneLarge} {
+              display: flex;
+              justify-content: space-between;
+            }
+          }
+          ul {
+            margin: 0;
+            font-size: 16px;
+            font-family: ${fonts.sans};
+            font-weight: ${weights.bold};
+            list-style: none;
+
+            &:last-of-type {
+              margin-right: 100px;
+            }
+
+            li {
+              margin-bottom: 4px;
+
+              &:before {
+                content: '—';
+                padding-right: 2px;
+              }
+            }
+          }
+
+          ${mediaQueries.phoneLarge} {
+            ${container.min}
+          }
+        `}
+      >
+        <h3>A leader and contributor since inception</h3>
         <p>
           We’re the only agency on Earth with a Drupal 8 core maintainer on our
           team. (Read: We know what we’re doing.)
         </p>
-        <span
-          css={css`
-            display: flex;
-          `}
-        >
+        <div>
           <ul>
             <li>Drupal 8 migration</li>
             <li>Replatform/redesign</li>
@@ -49,16 +117,21 @@ export default () => {
             <li>Ongoing support</li>
             <li>Training and resource augmentation</li>
           </ul>
-        </span>
+        </div>
       </FullWidthSection>
-      <ProjectsSlider />
-      <LogoGrid logoset='drupal' title='Some of Our Drupal Clients' />
+      <ProjectsSlider backgroundColor={colors.lightgray} />
+      <LogoGrid
+        logoset='drupal'
+        title='Some of Our Drupal Clients'
+        backgroundColor={colors.white}
+      />
       <SplitSection
-        css={css`
-          margin: 5rem auto;
-          width: 760px;
-        `}
         gridTemplateColumns='repeat(3, 1fr)'
+        css={css`
+          ${mediaQueries.phoneLarge} {
+            ${container.textOnly}
+          }
+        `}
       >
         <Tripple>
           <h1>800+</h1>
@@ -73,7 +146,31 @@ export default () => {
           <p>Building it as we speak</p>
         </Tripple>
       </SplitSection>
-      <SplitSection>
+      <SplitSection
+        gridColumnGap='16px'
+        css={css`
+          ${container.min}
+          img {
+            width: 100px;
+          }
+          h2 {
+            color: ${colors.reallydarkgray};
+            font-family: ${fonts.sans};
+            font-size: 21px;
+            font-weight: bold;
+            margin-bottom: 14px;
+          }
+          p {
+            font-weight: ${weights.light};
+            margin-bottom: 50px;
+            letter-spacing: -0.1px;
+          }
+
+          ${mediaQueries.phoneLarge} {
+            ${sectionPadding}
+          }
+        `}
+      >
         <article>
           <h2>Drupal 8</h2>
           <p>
@@ -110,6 +207,7 @@ export default () => {
         </article>
       </SplitSection>
       <Quote
+        size='small'
         data={{
           field_quote:
             'It’s a rare opportunity that an agency like TAG can fit in so well with our in-house team. The support, partnership, and commitment to creating a custom solution for our multi-lingual sites made all the difference in our successful launch.',
@@ -117,7 +215,7 @@ export default () => {
             'Steve Reichgut, Director of Web Engineering & Web Operations',
         }}
       />
-      <InsightsSlider />
+      <InsightsSlider showButton={false} />
     </Layout>
   );
 };
