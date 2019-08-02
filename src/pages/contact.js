0e65ea3450e6151cdce58e32a4876ee6c339a0c2
Abby Milberg <abby805@gmail.com>
2019-08-02 12:36:49 -0400

    Removing links from addresses.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -97,28 +97,16 @@ export default () => {
 
         <div css={contactItem}>
           <h3 css={headingCss}>Boston</h3>
-          <a
-            href='https://www.google.com/maps/place/1528+Webster+St,+Oakland,+CA+94612/@37.8047419,-122.2696821,17z/data=!3m1!4b1!4m5!3m4!1s0x808f80b364b1815d:0xd742e481761fb458!8m2!3d37.8047419!4d-122.2674934'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            333 Washington St Suite 326
-            <br />
-            Boston, MA 02108
-          </a>
+          333 Washington St Suite 326
+          <br />
+          Boston, MA 02108
         </div>
 
         <div css={contactItem}>
           <h3 css={headingCss}>Oakland</h3>
-          <a
-            href='https://www.google.com/maps/place/333+Washington+St,+Boston,+MA+02108/@42.3567997,-71.0633117,17z/data=!4m5!3m4!1s0x89e370849b9d3e3b:0xd5c1f9df0911e93!8m2!3d42.3569512!4d-71.059026'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            1528 Webster St
-            <br />
-            Oakland, CA 94612
-          </a>
+          1528 Webster St
+          <br />
+          Oakland, CA 94612
         </div>
 
         <div css={[contactItem, contactItemWide]}>
