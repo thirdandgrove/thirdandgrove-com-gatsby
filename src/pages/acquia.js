60540de948b93e58a9c6efb3fd209e46f04c2f67
Claire Annan <cannan@thirdandgrove.com>
2019-08-02 16:08:34 -0400

    Add light gray bg color to Acquia InsightSlider.

--- src/pages/acquia.js
+++ src/pages/acquia.js
@@ -199,7 +199,7 @@ export default () => {
             'Joe Wykes - SVP Global Channels & eCommerce - Acquia',
         }}
       />
-      <InsightsSlider showButton={false} />
+      <InsightsSlider showButton={false} backgroundColor={colors.lightgray} />
     </Layout>
   );
 };
