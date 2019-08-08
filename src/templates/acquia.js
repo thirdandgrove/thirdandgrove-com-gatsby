17c447623ba511b24e4eaa7decaf3b4742f0e370
Claire Annan <cannan@thirdandgrove.com>
2019-08-08 14:11:17 -0400

    Get rid of left padding on sub  ul.

--- src/templates/acquia.js
+++ src/templates/acquia.js
@@ -86,6 +86,7 @@ export default ({ pageContext }) => {
           }
           ul {
             margin: 0;
+            padding: 0;
             font-size: 16px;
             font-family: ${fonts.sans};
             font-weight: ${weights.bold};
