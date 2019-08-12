18bf2c2b64f693182988b2196e7cd08eb2d3775a
Abby Milberg <abby805@gmail.com>
2019-08-12 11:38:00 -0400

    T5-119: Cleaning up the Boston page's spacing

--- src/pages/boston.js
+++ src/pages/boston.js
@@ -8,7 +8,6 @@ import Image from 'gatsby-image';
 
 import Layout from '../components/layout';
 import FullWidthSection from '../components/FullWidthSection';
-import Text from '../components/ContentBody/Text';
 import { fonts, weights, contValues, mediaQueries, container } from '../styles';
 import SplitSection from '../components/SplitSection';
 
@@ -27,20 +26,28 @@ const Boston = ({ data }) => {
         textAlign='left'
         align='start'
         justify='start'
-        height='100%'
-        minHeight='100%'
+        height='auto'
+        minHeight='auto'
         css={css`
+          padding-top: 2rem;
+          ul {
+            padding: 0 0 2rem 0;
+            margin: 0;
+            li {
+              font-family: ${fonts.sans};
+              letter-spacing: 1px;
+              list-style: none;
+              padding: 0;
+            }
+          }
+
           ${mediaQueries.phoneLarge} {
-            padding: 0;
+            padding: 2rem 0 0;
             width: ${contValues.min};
           }
         `}
       >
-        <p
-          css={css`
-            padding-top: 2rem;
-          `}
-        >
+        <p>
           We&apos;re a full-service Boston-based agency with the best engineers
           in the city—any city really. We are the only company to employ a
           Drupal 8 core maintainer and consistently rank in the{' '}
@@ -48,35 +55,8 @@ const Boston = ({ data }) => {
             top 10 of Drupal agencies in the world.
           </a>
         </p>
-      </FullWidthSection>
-      <FullWidthSection
-        fontWeight={weights.thin}
-        margin='0 auto'
-        padding='0 20px'
-        textAlign='left'
-        align='start'
-        justify='start'
-        height='100%'
-        css={css`
-          ${mediaQueries.phoneLarge} {
-            padding: 0;
-            width: ${contValues.min};
-          }
-          ul {
-            margin: 0;
-            li {
-              font-family: ${fonts.sans};
-              letter-spacing: 1px;
-              list-style: none;
-            }
-          }
-        `}
-      >
-        <ul
-          css={css`
-            padding: 2rem 0;
-          `}
-        >
+
+        <ul>
           <li>
             <strong>Over 177,000 hours on Drupal 8.</strong> That&apos;s over
             7,375 days of our lives (talk about time well spent).
@@ -99,85 +79,50 @@ const Boston = ({ data }) => {
             MVP for our work on Acquia’s cloud APIs.
           </li>
         </ul>
-      </FullWidthSection>
-      <FullWidthSection
-        margin='0 auto'
-        padding='0 20px'
-        textAlign='left'
-        align='start'
-        justify='start'
-        minHeight='100%'
-        height='100%'
-        css={css`
-          ${mediaQueries.phoneLarge} {
-            padding: 0;
-            width: ${contValues.min};
-          }
-        `}
-      >
+
         <h2>Full Digital Coverage</h2>
-      </FullWidthSection>
-      <Text
-        data={{
-          field_body: {
-            processed: `
-          <h3> Digital Strategy</h3>
-          <p>
-            The foundation of success. We know what rocks to look under to drive
-            unique strategies for your business needs.
-          </p>
-          <h3>Creative</h3>
-          <p>
-            The connection to your brand experience. Reaching your goals is a hell
-            of a lot easier when you look good doing it.
-          </p>
-          <h3>Engineering</h3>
-          <p>
-            Our bread and butter. We’ve earned our technical chops and commerce
-            knowledge by working on some of the biggest commerce sites in the world.
-          </p>
-          `,
-          },
-        }}
-      />
-      <FullWidthSection
-        margin='0 auto'
-        padding='0 20px'
-        textAlign='left'
-        align='start'
-        justify='start'
-        height='100%'
-        minHeight='100%'
-        css={css`
-          ${mediaQueries.phoneLarge} {
-            padding: 0;
-            width: ${contValues.min};
-          }
-        `}
-      >
+
+        <h3> Digital Strategy</h3>
+
+        <p>
+          The foundation of success. We know what rocks to look under to drive
+          unique strategies for your business needs.
+        </p>
+
+        <h3>Creative</h3>
+
+        <p>
+          The connection to your brand experience. Reaching your goals is a hell
+          of a lot easier when you look good doing it.
+        </p>
+
+        <h3>Engineering</h3>
+
+        <p>
+          Our bread and butter. We’ve earned our technical chops and commerce
+          knowledge by working on some of the biggest commerce sites in the
+          world.
+        </p>
+
         <h2>We know our stuff but we’re not know-it-alls.</h2>
+
+        <p>
+          Two heads are better than one but a unified team is better than two.
+          We deliver the knowledge and horsepower but you bring the experience
+          that is needed for a thoughtful, reliable migration. It’s a
+          partnership and we like it that way.
+        </p>
+
+        <p>
+          Our experienced Architects will lead a thorough discovery process so
+          we can take full ownership of your project. With your in-depth
+          background and future needs mapped out, we’ll know exactly what to do
+          to maximize your Drupal project + ensure that you’ll be ready for D9.
+          Think of it this way—we usually support projects once we’re done so
+          it’s a lot easier for the both of us to do it right from the start.
+        </p>
       </FullWidthSection>
-      <Text
-        data={{
-          field_body: {
-            processed: `
-          <p>
-            Two heads are better than one but a unified team is better than two. We
-            deliver the knowledge and horsepower but you bring the experience that
-            is needed for a thoughtful, reliable migration. It’s a partnership and
-            we like it that way.
-          </p>
-          <p>
-            Our experienced Architects will lead a thorough discovery process so we
-            can take full ownership of your project. With your in-depth background
-            and future needs mapped out, we’ll know exactly what to do to maximize
-            your Drupal project + ensure that you’ll be ready for D9. Think of it
-            this way—we usually support projects once we’re done so it’s a lot
-            easier for the both of us to do it right from the start.
-          </p>`,
-          },
-        }}
-      />
+
       <SplitSection
         css={css`
           font-weight: ${weights.thin};
