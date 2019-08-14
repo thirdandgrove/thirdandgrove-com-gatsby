4b9958f0f217bf9b5cab23f5f97a6b61f062da54
Abby Milberg <abby805@gmail.com>
2019-08-14 15:38:07 -0400

    Increasing the global image quality (defaults to 50), see https://www.gatsbyjs.org/packages/gatsby-plugin-sharp/

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,12 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaultQuality: 100,
+      },
+    },
     `gatsby-plugin-emotion`,
     `gatsby-plugin-svgr`,
     {
