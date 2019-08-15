892f71b20033630b331233946d4eac25eafb95c1
Grant Glidewell <grant@thirdandgrove.com>
2019-08-15 10:40:02 -0700

    404 page (#254)
    * added 404 page
    
    * some  positioning

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import { navigate } from 'gatsby';
 
 import Layout from '../components/layout';
+import Button from '../components/Button';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout
+    headerData={{
+      height: '85vh',
+      mobileMinHeight: '85vh',
+      children: (
+        <span
+          css={css`
+            height: 60px;
+            width: 260px;
+            font-size: 16px;
+            font-weight: 300;
+            line-height: 27px;
+            text-align: center;
+            margin-top: -5rem;
+          `}
+        >
+          <h1>Damn.</h1>
+          <p>
+            That page is not available. Head home and we&apos;ll get you sorted.
+          </p>
+          <br />
+          <Button onClick={() => navigate('/')}>Go to home</Button>
+        </span>
+      ),
+    }}
+  >
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
 
