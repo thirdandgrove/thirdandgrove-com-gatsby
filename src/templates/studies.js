05156eb568e6ccc3f83313a6a56b03de913b5168
Abby Milberg <Abby805@gmail.com>
2019-08-19 11:34:07 -0400

    Fixing container width on case study subtitles. (#264)

--- src/templates/studies.js
+++ src/templates/studies.js
@@ -56,6 +56,7 @@ const Studies = ({ data }) => {
       )}
       <p
         css={css`
+          ${container.min};
           font-family: ${fonts.sans};
           font-size: 21px;
           font-weight: ${weights.bold};
@@ -64,7 +65,6 @@ const Studies = ({ data }) => {
           margin-bottom: 20px;
 
           ${mediaQueries.phoneLarge} {
-            ${container.min};
             letter-spacing: normal;
             padding: 75px 0 0;
             margin-bottom: 60px;
