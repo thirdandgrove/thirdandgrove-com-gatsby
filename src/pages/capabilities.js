513217094ae8fbc4ebefe3de1cbe2c014da87284
Abby Milberg <abby805@gmail.com>
2019-08-21 09:13:51 -0400

    Using px for consistency with the rest of the theme

--- src/pages/capabilities.js
+++ src/pages/capabilities.js
@@ -51,12 +51,8 @@ const Capability = ({ imageSrc, imageAlt, content, index, id }) => {
             }
 
             h2 {
-              font-size: 2.0625rem; /* 33px */
+              font-size: 33px;
               font-weight: ${weights.bold};
-
-              ${mediaQueries.phoneLarge} {
-                font-size: 1.83333rem; /* 33px with base 18 (112.5%) */
-              }
             }
 
             p {
