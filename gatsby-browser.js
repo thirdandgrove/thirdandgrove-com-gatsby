5e77f80deb8d9256e3676903328b058001142fd3
Grant Glidewell <grant@thirdandgrove.com>
2019-08-23 11:21:49 -0700

    rm unused files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import './src/styles/layout.css';
 
 const scrollTo = id => () => {
