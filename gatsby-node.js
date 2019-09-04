f0166cb9bbe82d62143adcee9ade7bb35501c93b
Grant Glidewell <grant@thirdandgrove.com>
2019-09-04 12:37:52 -0700

    trying a childprocess hack i found on a gatsby issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -510,3 +510,11 @@ exports.createPages = async ({ actions, graphql }) => {
     force: true,
   });
 };
+
+const ChildProcess = require('child_process');
+
+exports.onPostBuild = () => {
+  ChildProcess.execSync(
+    "ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill"
+  );
+};
