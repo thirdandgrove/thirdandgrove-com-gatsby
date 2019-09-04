269146f41906b82d1c98e7ff228e1e7913cfe02f
Grant Glidewell <grant@thirdandgrove.com>
2019-09-04 09:12:50 -0700

    minor update to test deploy in netlify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ fi
 
 COMMIT_MESSAGE=$(git log -1 --format="%h: %s")
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    netlify deploy --dir=public --functions=functions --prod --message="$COMMIT_MESSAGE"
+    netlify deploy --dir=public --prod --message="$COMMIT_MESSAGE"
 else
-    netlify deploy --dir=public --functions=functions --message="$COMMIT_MESSAGE"
+    netlify deploy --dir=public --message="$COMMIT_MESSAGE"
 fi
