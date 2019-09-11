ed65009ab327ccca829750408051a083395cfa6f
Grant Glidewell <grant@thirdandgrove.com>
2019-09-11 08:41:52 -0700

    updated hero line

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sitemap`,
-    { resolve: `gatsby-plugin-fixhash`, options: { offsetY: 20 } },
+    {
+      resolve: `gatsby-plugin-fixhash`,
+      options: { offsetY: 20 },
+    },
     {
       resolve: 'gatsby-plugin-react-axe',
       options: {
