f1e4c17c695b980633e6a50f7707d3e601337d43
Grant Glidewell <grant@thirdandgrove.com>
2019-11-14 12:09:07 -0800

    testing lambda function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,28 +1,106 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
+/* eslint-disable camelcase */
 require('dotenv').config();
 const axios = require('axios');
 
-const apiKey = process.env.KLAVIYO_API_KEY;
-const listId = process.env.KLAVIYO_LIST_ID; // update to not-suport@tag
-
-exports.handler = async (event, context, callback) => {
-  // ENDPOINT:
-  // https://a.klaviyo.com/api/v2/list/${listId}/subscribe
-  // REQUEST BODY:
-  //   {
-  //     "api_key": apiKey,
-  //     "profiles": [
-  //         {
-  //             "email": event.payload.email
-  //         }
-  //     ]
-  // }
-  // callback(null, {
-  //   statusCode: 200,
-  //   body: "Hello, World"
-  //   });
-  // parse event and handle contact form?
+exports.handler = async (event, _context, callback) => {
   const data = JSON.parse(event.body).payload;
-  console.log('DATA', JSON.stringify(data));
-  console.log('EMAIL', data.email);
+  const formName = data.form_name;
+  // if (formName === 'contact') {
+  //   // handle form contact
+  //   const { PIPEDRIVE_USER_ID, PIPEDRIVE_KEY } = process.env;
+  //   // pipedrive custom fields
+  //   const fields = {
+  //     dealDetails: '48bb1f3ddac751ddcca115d1340e6a47983d3687',
+  //     dealLeadSource: '30c4f33c7c7030fdd3360f5681851efb5de42712',
+  //     personWebsite: '7ec70f1ef58548a7555a66c6677cfd8028529568',
+  //   };
+  //   const {
+  //     name,
+  //     email,
+  //     phone,
+  //     website,
+  //     comments,
+  //     first_name,
+  //     last_name,
+  //   } = data;
+  //   let person;
+  //   let deal;
+  //   // check for required fields
+  //   if (!name || !email) {
+  //     console.error('error, no name or email sent');
+  //     return;
+  //   }
+
+  //   try {
+  //     person = await axios({
+  //       url: `https://api.pipedrive.com/v1/persons?api_token=${PIPEDRIVE_KEY}`,
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       data: JSON.stringify({
+  //         name,
+  //         first_name,
+  //         last_name,
+  //         email,
+  //         phone,
+  //         [fields.personWebsite]: website,
+  //         owner_id: PIPEDRIVE_USER_ID,
+  //       }),
+  //     });
+  //   } catch (err) {
+  //     console.log('error creating person', err);
+  //     callback(null, { statusCode: 200 });
+  //   }
+
+  //   const person_id = person.data && person.data.data.id;
+  //   try {
+  //     deal = await axios({
+  //       url: `https://api.pipedrive.com/v1/deals?api_token=${PIPEDRIVE_KEY}`,
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       data: JSON.stringify({
+  //         title: `[Webform] Deal for ${name} <${email}>`,
+  //         [fields.dealLeadSource]: '34',
+  //         person_id,
+  //         [fields.dealDetails]: comments,
+  //         user_id: PIPEDRIVE_USER_ID,
+  //       }),
+  //     });
+  //   } catch (err) {
+  //     console.error('error creating deal', err);
+  //     callback(null, { statusCode: 200 });
+  //   }
+
+  //   const deal_id = deal.data && deal.data.data.id;
+  //   try {
+  //     await axios({
+  //       url: `https://api.pipedrive.com/v1/notes?api_token=${PIPEDRIVE_KEY}`,
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       data: JSON.stringify({
+  //         content: website,
+  //         deal_id,
+  //       }),
+  //     });
+  //   } catch (err) {
+  //     console.error('error creating note', err);
+  //     callback(null, { statusCode: 200 });
+  //   }
+  // }
+
+  if (formName === 'newsleter') {
+    // handle form newsletter
+    const { KLAVIYO_API_KEY, KLAVIYO_LIST_ID } = process.env;
+    const { email } = data;
+    const formData = new FormData();
+    formData.append('api_key', KLAVIYO_API_KEY);
+    formData.append('email', email);
+    await axios({
+      url: `https://a.klaviyo.com/api/v2/list/${KLAVIYO_LIST_ID}/subscribe`,
+      data: formData,
+    });
+  }
+
   callback(null, { statusCode: 200 });
 };
