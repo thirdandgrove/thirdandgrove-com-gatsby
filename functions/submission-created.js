4ace52427f9998b8d2d3febd71e6924d1eeb8ee3
Grant Glidewell <grant@thirdandgrove.com>
2019-11-15 11:54:00 -0800

    lets get this data and get to the weekend

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -16,83 +16,82 @@ exports.handler = async (event, _context, callback) => {
       dealLeadSource: '30c4f33c7c7030fdd3360f5681851efb5de42712',
       personWebsite: '7ec70f1ef58548a7555a66c6677cfd8028529568',
     };
-    const {
-      name,
-      email,
-      phone,
-      website,
-      comments,
-      first_name,
-      last_name,
-    } = data;
-    console.log(data);
-    let person;
-    let deal;
-    // check for required fields
-    if (!name || !email || !website) {
-      console.error('error, no name, email or website sent.', {
-        name,
-        email,
-        website,
-      });
-      return;
-    }
+    const humanFields = data.ordered_human_fields.reduce((acc, item) => {
+      acc.name = item.value;
+      return acc;
+    }, {});
+    const { name, email, phone, website, comments } = humanFields;
+    const first_name = name.split(' ')[0];
+    const last_name = name.split(' ')[name.split(' ').length - 1];
+    console.log(humanFields, data.first_name, data.last_name);
+    return; // for now lets not pollute a bunch of deals
+    // let person;
+    // let deal;
+    // // check for required fields
+    // if (!name || !email || !website) {
+    //   console.error('error, no name, email or website sent.', {
+    //     name,
+    //     email,
+    //     website,
+    //   });
+    //   return;
+    // }
 
-    try {
-      person = await axios({
-        url: `https://api.pipedrive.com/v1/persons?api_token=${PIPEDRIVE_KEY}`,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        data: JSON.stringify({
-          name,
-          first_name,
-          last_name,
-          email,
-          phone,
-          [fields.personWebsite]: website,
-          owner_id: PIPEDRIVE_USER_ID,
-        }),
-      });
-    } catch (err) {
-      console.log('error creating person', err);
-      callback(null, { statusCode: 200 });
-    }
+    // try {
+    //   person = await axios({
+    //     url: `https://api.pipedrive.com/v1/persons?api_token=${PIPEDRIVE_KEY}`,
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     data: JSON.stringify({
+    //       name,
+    //       first_name,
+    //       last_name,
+    //       email,
+    //       phone,
+    //       [fields.personWebsite]: website,
+    //       owner_id: PIPEDRIVE_USER_ID,
+    //     }),
+    //   });
+    // } catch (err) {
+    //   console.log('error creating person', err);
+    //   callback(null, { statusCode: 200 });
+    // }
 
-    const person_id = person.data && person.data.data.id;
-    try {
-      deal = await axios({
-        url: `https://api.pipedrive.com/v1/deals?api_token=${PIPEDRIVE_KEY}`,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        data: JSON.stringify({
-          title: `[Webform] Deal for ${name} <${email}>`,
-          [fields.dealLeadSource]: '34',
-          person_id,
-          [fields.dealDetails]: comments,
-          user_id: PIPEDRIVE_USER_ID,
-        }),
-      });
-    } catch (err) {
-      console.error('error creating deal', err);
-      callback(null, { statusCode: 200 });
-    }
+    // const person_id = person.data && person.data.data.id;
+    // try {
+    //   deal = await axios({
+    //     url: `https://api.pipedrive.com/v1/deals?api_token=${PIPEDRIVE_KEY}`,
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     data: JSON.stringify({
+    //       title: `[Webform] Deal for ${name} <${email}>`,
+    //       [fields.dealLeadSource]: '34',
+    //       person_id,
+    //       [fields.dealDetails]: comments,
+    //       user_id: PIPEDRIVE_USER_ID,
+    //     }),
+    //   });
+    // } catch (err) {
+    //   console.error('error creating deal', err);
+    //   callback(null, { statusCode: 200 });
+    // }
 
-    const deal_id = deal.data && deal.data.data.id;
-    console.log('DEAL CREATED, ADDING NOTE:', website, data, deal.data.data);
-    try {
-      await axios({
-        url: `https://api.pipedrive.com/v1/notes?api_token=${PIPEDRIVE_KEY}`,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        data: JSON.stringify({
-          content: website,
-          deal_id,
-        }),
-      });
-    } catch (err) {
-      console.error('error creating note', err);
-      callback(null, { statusCode: 200 });
-    }
+    // const deal_id = deal.data && deal.data.data.id;
+    // console.log('DEAL CREATED, ADDING NOTE:', website, data, deal.data.data);
+    // try {
+    //   await axios({
+    //     url: `https://api.pipedrive.com/v1/notes?api_token=${PIPEDRIVE_KEY}`,
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     data: JSON.stringify({
+    //       content: website,
+    //       deal_id,
+    //     }),
+    //   });
+    // } catch (err) {
+    //   console.error('error creating note', err);
+    //   callback(null, { statusCode: 200 });
+    // }
   }
 
   if (form_name === 'newsletter') {
