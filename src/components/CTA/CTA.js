a43e61933a51c97317da958f9db6e17905210cad
Sarah Yoder <syoder@thirdandgrove.com>
2019-11-27 15:29:19 -0500

    swat-1354: For now, we don't want the tagline.

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -7,11 +7,12 @@ import { colors } from '../../styles';
 import FullWidthSection from '../FullWidthSection';
 import Button from '../Button';
 
-const CTA = ({ backgroundColor, headline, tagline, cta, link }) => {
+const CTA = ({ backgroundColor, headline, cta, link }) => {
   return (
     <FullWidthSection
       backgroundColor={backgroundColor}
       minHeight='300px'
+      height='300px'
       align-items='center'
       css={css`
         padding-top: 40px;
@@ -27,16 +28,6 @@ const CTA = ({ backgroundColor, headline, tagline, cta, link }) => {
       >
         {headline}
       </h1>
-      <p
-        css={css`
-          color: ${colors.reallydarkgray}
-          font-size: 16px;
-          font-weight: 300;
-          text-align: center;
-        `}
-      >
-        {tagline}
-      </p>
       <Button onClick={() => navigate(link)}>{cta}</Button>
     </FullWidthSection>
   );
@@ -45,7 +36,6 @@ const CTA = ({ backgroundColor, headline, tagline, cta, link }) => {
 CTA.propTypes = {
   backgroundColor: PropTypes.string,
   headline: PropTypes.string,
-  tagline: PropTypes.string,
   cta: PropTypes.string,
   link: PropTypes.string,
 };
@@ -53,7 +43,6 @@ CTA.propTypes = {
 CTA.defaultProps = {
   backgroundColor: colors.lightblue,
   headline: `Want to catch up over coffee?`,
-  tagline: `We're located in Downtown Crossing but enjoy putting in some miles to get around the city.`,
   cta: `GET IN TOUCH`,
   link: `/contact/`,
 };
