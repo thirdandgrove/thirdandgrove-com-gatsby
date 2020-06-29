40c149e6f722fbb1abcc8b7485244e96a5d98aae
naeluh <naeluh@gmail.com>
2020-06-29 16:49:45 -0700

    Add phone video component to start

---
+++ src/components/PhoneVideo/PhoneVideo.js
@@ -0,0 +1,6 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+
+export default () => {};
