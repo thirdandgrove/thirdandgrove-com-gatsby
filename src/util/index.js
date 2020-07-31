46db2ef0e600fde17e0b6282d38fdb5fc18478d8
naeluh <naeluh@gmail.com>
2020-07-31 16:24:22 -0700

    T5-212 Add function that converts node/nid to path alias

--- src/util/index.js
+++ src/util/index.js
@@ -5,4 +5,49 @@ module.exports = {
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
       .join('&');
   },
+  updatePaths: json => {
+    return new Promise((resolve, reject) => {
+      return [json.data].map(
+        ({ caseStudies, insights, legacyInsights, redirects }) => {
+          if (!caseStudies || !insights || !legacyInsights || !redirects) {
+            const error = new Error('missing dependency');
+            reject(error);
+            throw error;
+          }
+
+          const caseStudyNodeId = caseStudies.nodes;
+          const insightsNodeId = insights.nodes;
+          const legacyInsightsNodeId = legacyInsights.nodes;
+          const updatedRedirects = [];
+          const nodeArray = caseStudyNodeId.concat(
+            insightsNodeId,
+            legacyInsightsNodeId
+          );
+
+          redirects.edges.forEach(({ node }) => {
+            if (node.redirect_redirect.uri.indexOf('/node/') !== -1) {
+              const nid = node.redirect_redirect.uri.replace(
+                'internal:/node/',
+                ''
+              );
+
+              nodeArray.forEach(value => {
+                if (Number(nid) === value.drupal_internal__nid) {
+                  node.redirect_redirect.uri = value.path.alias;
+                }
+              });
+            }
+            updatedRedirects.push({
+              node,
+            });
+          });
+
+          redirects.edges = updatedRedirects;
+
+          resolve(redirects);
+          return redirects;
+        }
+      );
+    });
+  },
 };
