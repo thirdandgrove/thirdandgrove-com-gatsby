2f4f2ae5dc40c68c82ab8986ae5cdd3d9f93fd74
naeluh <naeluh@gmail.com>
2020-09-14 17:41:25 -0700

    T5-234 Update CTA Grid component

--- src/components/CTAGrid/CTAGrid.js
+++ src/components/CTAGrid/CTAGrid.js
@@ -1,25 +1,111 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+
+import { container, mediaQueries, weights, fonts, colors } from '../../styles';
+import FullWidthSection from '../FullWidthSection';
+import { FakeButton } from '../Button';
 
 import CTAGridItem from './CTAGridItem';
 
-const CTAGrid = ({ items }) => {
+const CTAGrid = ({ items, images, header, backgroundColor, altStyle }) => {
+  const ctaGridContainer = css`
+    display: grid;
+    -ms-grid-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 5%;
+    grid-row-gap: 70px;
+    width: 100%;
+    padding-bottom: 36px;
+
+    ${mediaQueries.phoneLarge} {
+      ${container.textOnly}
+      display: grid;
+      -ms-grid-columns: 1fr 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-column-gap: 70px;
+      grid-row-gap: 70px;
+      padding-bottom: 24px;
+    }
+  `;
+
+  const ctaGridContainerAlt = css`
+    display: block;
+
+    ${mediaQueries.phoneLarge} {
+      ${container.textOnly}
+      display: grid;
+      -ms-grid-columns: 1fr 1fr;
+      grid-template-columns: 1fr 1fr;
+      grid-column-gap: 100px;
+    }
+  `;
+
+  const sectionPadding = css`
+    padding: 100px 20px;
+
+    ${mediaQueries.phoneLarge} {
+      padding: 125px 0;
+    }
+  `;
+
+  const sectionStyles = css`
+    ${sectionPadding}
+
+    clear: both;
+
+    h3 {
+      font-size: 27px;
+      text-align: center;
+      padding-bottom: 24px;
+
+      ${mediaQueries.phoneLarge} {
+        width: 820px;
+        max-width: 100%;
+        margin: 0 auto;
+        font-size: 39px;
+        padding-bottom: 48px;
+      }
+    }
+  `;
+  const getImageSrc = name => images.filter(({ node }) => name === node.name);
   return (
-    <div>
-      {items.map(({ icon, text }) => (
-        <CTAGridItem icon={icon} text={text} />
-      ))}
-    </div>
+    <FullWidthSection css={sectionStyles} backgroundColor={backgroundColor}>
+      {header !== '' && <h3>{header}</h3>}
+      <div css={!altStyle ? ctaGridContainer : ctaGridContainerAlt}>
+        {items.map(({ node }) => (
+          <CTAGridItem
+            icon={getImageSrc(node.icon)}
+            title={node.title}
+            description={node.description}
+            altStyle={altStyle}
+          />
+        ))}
+      </div>
+      {!altStyle && (
+        <Link to='http://google.com'>
+          <FakeButton>Get Support Now</FakeButton>
+        </Link>
+      )}
+    </FullWidthSection>
   );
 };
 
 CTAGrid.propTypes = {
   items: PropTypes.array,
+  images: PropTypes.array,
+  altStyle: PropTypes.bool,
+  header: PropTypes.string,
+  backgroundColor: PropTypes.string,
 };
 
 CTAGrid.defaultProps = {
   items: [],
+  images: [],
+  altStyle: false,
+  header: '',
+  backgroundColor: '#FFF',
 };
 
 export default CTAGrid;
--- src/components/CTAGrid/CTAGridItem.js
+++ src/components/CTAGrid/CTAGridItem.js
@@ -2,18 +2,85 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
-const CTAGridItem = ({ icon, text }) => {
-  return <div />;
+import { container, mediaQueries, weights, fonts, colors } from '../../styles';
+
+const CTAGridItem = ({ icon, title, description, altStyle }) => {
+  const ctaContainer = css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    img {
+      margin-bottom: 48px;
+      max-height: 57px;
+    }
+
+    h4 {
+      font-size: 27px;
+      ${mediaQueries.phoneLarge} {
+        margin-bottom: 36px;
+        font-size: 39px;
+        line-height: 48px;
+      }
+    }
+
+    p {
+      font-weight: ${weights.thin};
+    }
+  `;
+
+  const ctaContainerAlt = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 24px 0;
+    border-bottom: none;
+
+    ${mediaQueries.phoneLarge} {
+      border-bottom: 0.25px solid rgba(41, 41, 42, 0.2);
+    }
+
+    img {
+      margin-right: 12px;
+      margin-bottom: 0;
+    }
+
+    h4 {
+      font-family: ${fonts.sans};
+      font-size: 16px;
+      font-weight: ${weights.bold};
+      margin: 0;
+    }
+
+    p {
+    }
+
+    &:nth-last-child(2),
+    &:last-of-type {
+      border-bottom: none;
+    }
+  `;
+  return (
+    <div css={!altStyle ? ctaContainer : ctaContainerAlt}>
+      <img src={icon[0].node.publicURL} alt={description} />
+      <h4>{title}</h4>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 CTAGridItem.propTypes = {
   icon: PropTypes.string,
-  text: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  altStyle: PropTypes.bool,
 };
 
 CTAGridItem.defaultProps = {
   icon: '',
-  text: '',
+  title: '',
+  description: '',
+  altStyle: false,
 };
 
 export default CTAGridItem;
