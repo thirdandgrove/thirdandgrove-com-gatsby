244749d425a437a4f8c5c8aee96469ad91f8f9c7
naeluh <naeluh@gmail.com>
2020-09-16 17:25:51 -0700

    T5-234 Bring back logo

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -18,7 +18,36 @@ const TopNav = ({ fill, hideNav }) => {
   const { width } = useWindow();
   return (
     <>
-      {!hideNav && (
+      {hideNav ? (
+        <div
+          css={[
+            container.max,
+            css`
+              position: ${isOpen ? 'fixed' : 'absolute'};
+              top: 0;
+              padding-top: 20px;
+              padding-bottom: 10px;
+              left: 50%;
+              transform: translateX(-50%);
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              z-index: 4;
+
+              ${mediaQueries.phoneLarge} {
+                padding-top: 30px;
+              }
+            `,
+          ]}
+        >
+          <ThirdAndGrove
+            css={css`
+              height: 22px;
+              fill: ${isOpen ? colors.lightgray : fill};
+            `}
+          />
+        </div>
+      ) : (
         <>
           {' '}
           <div
