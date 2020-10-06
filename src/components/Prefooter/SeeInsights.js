41eeddb2078ddb76a4b48d034abe8a44a52e0ba7
naeluh <naeluh@gmail.com>
2020-10-06 13:07:19 -0700

    T5-236 Add prefooter

---
+++ src/components/Prefooter/SeeInsights.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+import { css } from '@emotion/core';
+
+import Button from '../Button';
+import { colors, mediaQueries } from '../../styles';
+
+import TextWrapper from './TextWrapper';
+
+const wrapperStyles = css`
+  ${mediaQueries.phoneLarge} {
+    min-height: 500px;
+  }
+`;
+
+const SeeInsights = () => (
+  <TextWrapper backgroundColor={colors.lightblue} css={wrapperStyles}>
+    <h3>Explore our Insights.</h3>
+    <Button onClick={() => navigate(`/insights/`)}>Insights</Button>
+  </TextWrapper>
+);
+
+export default SeeInsights;
