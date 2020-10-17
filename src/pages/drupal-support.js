3bd190328a61291854c50e721e783d1bf5c64566
naeluh <naeluh@gmail.com>
2020-10-16 18:20:57 -0700

    Fix Anchor links in CTAs

--- src/pages/drupal-support.js
+++ src/pages/drupal-support.js
@@ -112,7 +112,7 @@ const DrupalSupport = ({ data }) => {
         headline='Catering to complex and integrated Drupal environments.'
         subTitle='Managed Services offering leading 24x7 support, maintenance, and optimization to mid-market organizations'
         cta='GET SUPPORT NOW'
-        link='/drupal-support#contact'
+        link='/drupal-support/#contact'
         icon={drupalLogo}
         iconAlt='Drupal'
         altStyle
@@ -170,7 +170,7 @@ const DrupalSupport = ({ data }) => {
       <CTAGrid
         items={data.allDrupalSupportCtaGridOneJson.edges}
         images={data.allFile.edges}
-        link='/drupal-support#contact'
+        link='/drupal-support/#contact'
         cta='Get Support Now'
         altStyle={false}
       />
