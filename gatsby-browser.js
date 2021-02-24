a255f2362bda1e8493a5786fea9470f62adf55b6
naeluh <naeluh@gmail.com>
2021-02-23 16:41:35 -0800

    T5-197 Add gatsby-browser to use polyfills so gatsby develop run in ie11

---
+++ gatsby-browser.js
@@ -0,0 +1,29 @@
+/* eslint-disable global-require */
+// Check if production branch
+// We don't need to add these to production bundle
+const isProduction =
+  process.env.BRANCH !== undefined
+    ? process.env.BRANCH !== 'master'
+      ? 'development'
+      : 'production'
+    : 'development';
+
+// Set polyfills for ie11 development
+if (!isProduction) {
+  // Sets babel-polyfill for ie11
+  require('babel-polyfill');
+  // Sets shadow dom polyfill for ie11
+  require('@webcomponents/shadydom');
+  // Sets EventSource polyfill for ie11
+  const {
+    NativeEventSource,
+    EventSourcePolyfill,
+  } = require('event-source-polyfill');
+  global.EventSource = NativeEventSource || EventSourcePolyfill;
+}
+
+exports.onClientEntry = () => {
+  // Don't need to do anything here, but if you don't
+  // export something, the import won't work.
+};
+/* eslint-disable global-require */
