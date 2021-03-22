ca3e1f8e22f7b56657ca0b1be99ed1793d4ef76c
elite0226 <elite0226@outlook.com>
2021-03-22 07:25:02 -0400

    Add badges to partner page

--- src/pages/partners/index.js
+++ src/pages/partners/index.js
@@ -1,12 +1,88 @@
 import React from 'react';
+import { navigate } from 'gatsby';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import Layout from '../../components/layout';
 import FullWidthSection from '../../components/FullWidthSection';
-import LogoGrid from '../../components/LogoGrid';
-import { colors, mediaQueries } from '../../styles';
+import { mediaQueries } from '../../styles';
+import DrupalBadge from '../../../static/images/drupal-badge.svg';
+import AcquiaBadge from '../../../static/images/acquia-badge.svg';
+import ShopifyBadge from '../../../static/images/shopify-badge.svg';
+import GatsbyBadge from '../../../static/images/gatsby-badge.png';
+import BigcommerceBadge from '../../../static/images/bigcommerce-badge.png';
 
 const Partners = () => {
+  const badges = [
+    {
+      src: AcquiaBadge,
+      alt: 'acquia',
+      label: 'Acquia',
+      link: '/partners/acquia',
+    },
+    {
+      src: DrupalBadge,
+      alt: 'drupal',
+      label: 'Drupal',
+      link: '/partners/drupal',
+    },
+    {
+      src: GatsbyBadge,
+      alt: 'gatsby',
+      label: 'Gatsby',
+      link: '/partners/gatsby',
+    },
+    {
+      src: ShopifyBadge,
+      alt: 'shopify',
+      label: 'Shopify',
+      link: '/partners/shopify',
+    },
+    {
+      src: BigcommerceBadge,
+      alt: 'bigcommerce',
+      label: 'Big Commerce',
+      link: '/partners/big-commerce',
+    },
+  ];
+
+  const handleGoTo = link => () => {
+    navigate(link);
+  };
+
+  const Badges = styled.div`
+    max-width: 850px;
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+  `;
+
+  const badgeItem = css`
+    width: 50%;
+    max-width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    flex: 0 0 50%;
+    margin-bottom: 70px;
+    cursor: pointer;
+
+    ${mediaQueries.tablet} {
+      flex: 0 0 33%;
+      width: 33%;
+      max-width: 33%;
+    }
+
+    img {
+      max-width: 180px;
+      height: auto;
+    }
+  `;
+
   return (
     <Layout
       headerData={{
@@ -27,11 +103,15 @@ const Partners = () => {
           }
         `}
       />
-      <LogoGrid
-        title='Our Partners'
-        logoset='all'
-        backgroundColor={colors.white}
-      />
+      <Badges>
+        {badges.map((badge, index) => (
+          // eslint-disable-next-line
+          <div key={index} css={badgeItem} onClick={handleGoTo(badge.link)}>
+            <img src={badge.src} alt={badge.alt} />
+            <h4>{badge.label}</h4>
+          </div>
+        ))}
+      </Badges>
     </Layout>
   );
 };
