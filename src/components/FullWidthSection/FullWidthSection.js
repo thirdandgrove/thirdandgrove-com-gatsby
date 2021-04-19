d3582759dabd0b736801ae5033bc4da59fe2da6a
Shane Thomas <shane@codekarate.com>
2021-04-19 00:10:20 -0500

    Add additional accessibility audit fixes

--- src/components/FullWidthSection/FullWidthSection.js
+++ src/components/FullWidthSection/FullWidthSection.js
@@ -2,35 +2,27 @@ import styled from '@emotion/styled';
 
 import { weights, mediaQueries } from '../../styles';
 
-const FullWidthSection = props => {
-  const sectionBackgroundCSS =
-    props && props.backgroundImage
-      ? styled.section`
-          background-image: url(${props.backgroundImage});
-        `
-      : styled.section``;
+export default styled.section`
+  width: ${props => props.width || '100%'};
+  min-height: ${props => props.minHeight || '300px'};
+  display: flex;
+  font-weight: ${props => props.fontWeight || weights.medium};
+  flex-direction: ${props => props.flexDirection || 'column'};
+  align-items: ${props => props.align || 'center'};
+  justify-content: ${props => props.justify || 'center'};
+  background-color: ${props => props.backgroundColor};
+  ${props =>
+    props.backgroundImage
+      ? `background-image: url(${props.backgroundImage})`
+      : ``};
+  background-size: cover;
+  overflow: hidden;
+  background-position: center;
+  margin: ${props => props.margin || '0 auto'};
+  padding: ${props => props.padding || '0 20px'};
+  text-align: ${props => props.textAlign || 'left'};
 
-  return styled.section`
-    width: ${props.width || '100%'};
-    min-height: ${props.minHeight || '300px'};
-    display: flex;
-    font-weight: ${props.fontWeight || weights.medium};
-    flex-direction: ${props.flexDirection || 'column'};
-    align-items: ${props.align || 'center'};
-    justify-content: ${props.justify || 'center'};
-    background-color: ${props.backgroundColor};
-    background-size: cover;
-    overflow: hidden;
-    background-position: center;
-    margin: ${props.margin || '0 auto'};
-    padding: ${props.padding || '0 20px'};
-    text-align: ${props.textAlign || 'left'};
-    ${sectionBackgroundCSS}
-
-    ${mediaQueries.phoneLarge} {
-      min-height: ${props.height || '700px'};
-    }
-  `;
-};
-
-export default FullWidthSection;
+  ${mediaQueries.phoneLarge} {
+    min-height: ${props => props.height || '700px'};
+  }
+`;
