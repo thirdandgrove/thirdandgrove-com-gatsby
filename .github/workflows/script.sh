54c8f4bb9ad13b990b21adaab0cb97114cb1d5c0
naeluh <naeluh@gmail.com>
2021-04-20 14:46:48 -0700

    T5-253 Add bash script

---
+++ .github/workflows/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+recurse()
+{
+status=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[0].status')
+
+echo $status
+
+if [ $status == "completed" ]; then
+  echo "Waiting on check to complete ... "
+  echo $1
+  recurse $1
+  echo $status
+else
+  echo "> Check has completed"
+  echo "> Check Gatsby build conclusion"
+  echo $status
+  local conclusion=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[0].conclusion')
+  echo $conclusion
+
+  if [ $conclusion == "success" ]; then
+    echo $conclusion == "success"
+  else
+    echo $conclusion
+  fi
+
+fi
+}
+
+recurse $1
