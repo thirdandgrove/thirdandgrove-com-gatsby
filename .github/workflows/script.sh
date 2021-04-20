7ff1fc17efa2966dda2e1899eebb883b64f27a96
naeluh <naeluh@gmail.com>
2021-04-20 16:23:11 -0700

    update string matching

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,14 +4,14 @@ recurse()
 {
 status=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[0].status')
 
-if [ $status == "completed" ]; then
+if [[ $status == *"completed"* ]]; then
   echo "> Check has completed"
   echo "> Check Gatsby build conclusion"
   echo $status
   local conclusion=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[0].conclusion')
   echo $conclusion
 
-  if [ $conclusion == "success" ]; then
+  if [[ $conclusion == *"success"* ]]; then
     echo $conclusion == "success"
   else
     echo "> Gatsby build conclusion was not successful"
@@ -20,7 +20,7 @@ if [ $status == "completed" ]; then
   fi
 
 else
-  sleep 20s
+  sleep 10s
   echo "Waiting on check to complete ... "
   echo $status
   recurse $1
