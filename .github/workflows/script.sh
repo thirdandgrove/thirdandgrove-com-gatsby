e9f0bb03b40363aa44a80430a3a165807caa3dc0
naeluh <naeluh@gmail.com>
2021-04-20 18:24:22 -0700

    Filter curl call

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,14 +2,15 @@
 
 recurse()
 {
-status=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[1].status')
-name=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[1].name')
+status=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq -c -r '.check_runs[] | select( .name | contains("Gatsby Build Service - test-build-tagd8")) | .status')
+name=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq -c -r '.check_runs[] | select( .name | contains("Gatsby Build Service - test-build-tagd8")) | .name')
+
 
 if [[ $status == *"completed"* ]]; then
   echo "> Check has completed"
   echo "> Check Gatsby build conclusion"
   echo $status
-  local conclusion=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq '.check_runs[1].conclusion')
+  local conclusion=$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$1/check-runs | jq -c '.check_runs[] | select( .name | contains("Gatsby Build Service - test-build-tagd8")) | .conclusion')
   echo $conclusion
 
   if [[ $conclusion == *"success"* ]]; then
