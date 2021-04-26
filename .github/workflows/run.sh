be5cadfb574886518cd00b1e4310cd5b1e6566db
naeluh <naeluh@gmail.com>
2021-04-25 19:46:20 -0700

    echo debug

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "$SHA"
+
 buildUrl=https://build-$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$SHA/check-runs | jq -c -r '.check_runs[]? | select( .name | contains("Gatsby Build Service - tagd8_gatsby")) | .external_id').gtsb.io/
 
 echo "$buildUrl"
