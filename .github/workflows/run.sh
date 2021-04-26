de73a5f2ed0025573dc7c9d480d20a6e7a771f21
naeluh <naeluh@gmail.com>
2021-04-25 19:44:36 -0700

    build url in command

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-NODE_ENV=development CYPRESS_BASE_URL=$CYPRESS_BASE_URL cypress run
+buildUrl=https://build-$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$SHA/check-runs | jq -c -r '.check_runs[]? | select( .name | contains("Gatsby Build Service - tagd8_gatsby")) | .external_id').gtsb.io/
+
+echo "$buildUrl"
+
+NODE_ENV=development CYPRESS_BASE_URL=$buildUrl cypress run
