0741f50d9603cdd4c4b2c21d778dcac890d575db
naeluh <naeluh@gmail.com>
2021-04-26 11:24:12 -0700

    T5-253 Remove debug and set node version to 12 and majors

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "$SHA"
-
 buildUrl=https://build-$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$SHA/check-runs | jq -c -r '.check_runs[]? | select( .name | contains("Gatsby Build Service - tagd8_gatsby")) | .external_id').gtsb.io/
-
-echo "$buildUrl"
 
 NODE_ENV=test CYPRESS_BASE_URL=$buildUrl cypress run
