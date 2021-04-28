3d76dc531d199980803aa5dbc8f18976586df5b2
naeluh <naeluh@gmail.com>
2021-04-28 14:41:17 -0700

    T5-281 Remove unused imports

--- src/components/CTAGrid/CTAGrid.js
+++ src/components/CTAGrid/CTAGrid.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/react';
 import { Link } from 'gatsby';
