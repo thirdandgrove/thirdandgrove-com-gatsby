3ef01e06f8cdd011fd68f3c618b87d0926dafc50
naeluh <naeluh@gmail.com>
2021-05-07 14:41:44 -0700

    T5-253 Add multiple browsers

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -2,4 +2,4 @@
 
 buildUrl=https://build-$(curl -s  https://api.github.com/repos/thirdandgrove/thirdandgrove-com-gatsby/commits/$SHA/check-runs | jq -c -r '.check_runs[]? | select( .name | contains("Gatsby Build Service - tagd8_gatsby")) | .external_id').gtsb.io/
 
-NODE_ENV=test CYPRESS_BASE_URL=$buildUrl nyc --reporter=html cypress run
+NODE_ENV=test CYPRESS_BASE_URL=$buildUrl nyc --reporter=html cypress run --browser $BROWSER
