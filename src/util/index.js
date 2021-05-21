da4f0c81197542efa88ba8416641b1a297175c20
naeluh <naeluh@gmail.com>
2021-05-21 16:54:51 -0700

    T5-283 Add util function

--- src/util/index.js
+++ src/util/index.js
@@ -5,6 +5,40 @@ module.exports = {
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
       .join('&');
   },
+  modifyExternalLinks: html => {
+    const checkDomain = url => {
+      let u = url;
+      if (u.indexOf('//') === 0) {
+        u = window.location.protocol + u;
+      }
+      return u
+        .toLowerCase()
+        .replace(/([a-z])?:\/\//, '$1')
+        .split('/')[0];
+    };
+
+    const isExternalURL = url => {
+      return (
+        ((url.length > 1 && url.indexOf(':') > -1) || url.indexOf('//') > -1) &&
+        checkDomain(window.location.href) !== checkDomain(url)
+      );
+    };
+
+    const parser = new DOMParser();
+    const content = parser.parseFromString(html, 'text/html');
+    const anchors = [...content.getElementsByTagName('a')];
+
+    anchors.forEach(v => {
+      const href = v.getAttribute('href');
+
+      if (isExternalURL(href)) {
+        v.setAttribute('target', '_blank');
+        v.setAttribute('rel', 'noreferrer');
+      }
+    });
+
+    return content.body.innerHTML;
+  },
   updatePaths: json => {
     return new Promise((resolve, reject) => {
       return [json.data].map(
