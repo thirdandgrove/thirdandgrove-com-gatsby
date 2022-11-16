b79d9d74c61dd7901ade92facfc9b117e470a591
Sam Seide <sseide@thirdandgrove.com>
2022-11-16 12:08:33 -0600

    add more breakpoints to slider items

--- src/pages/about.js
+++ src/pages/about.js
@@ -296,14 +296,28 @@ const About = ({ data }) => {
       {
         breakpoint: 20000,
         settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
           slidesToShow: 4,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 900,
+        breakpoint: 750,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
