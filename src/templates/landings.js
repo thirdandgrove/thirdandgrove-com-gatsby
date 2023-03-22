72bb6415b7c60227b659c6e199eea7f61336b476
RonaldChicaVera <rchica@rootstack.com>
2023-03-22 15:12:31 -0500

    T5-355: add union types and fix slider

--- src/templates/landings.js
+++ src/templates/landings.js
@@ -138,9 +138,6 @@ export const query = graphql`
           name: display_name
         }
         field_components {
-          ... on component__big_links {
-            id
-          }
           ... on component__logo_wall {
             id
             field_header_text
@@ -250,98 +247,222 @@ export const query = graphql`
                 name
               }
               field_content {
-                id
-                title
-                field_inverse_header
-                field_image {
-                  alt
-                }
-                created(formatString: "MMM D, YYYY")
-                path {
-                  alias
-                }
-                relationships {
-                  node_type {
-                    name
-                  }
-                  uid {
-                    name: display_name
-                  }
-                  field_e_book_file {
-                    filename
-                    id
-                    localFile {
-                      publicURL
-                    }
-                  }
+                type: __typename
+                ... on insight {
+                  id
+                  title
+                  field_inverse_header
                   field_image {
-                    id
-                    localFile {
-                      publicURL
-                      childImageSharp {
-                        fluid(maxWidth: 530, maxHeight: 400) {
-                          ...GatsbyImageSharpFluid_withWebp
-                        }
-                        gatsbyImageData(
-                          height: 400
-                          width: 530
-                          transformOptions: { cropFocus: CENTER }
-                          layout: FULL_WIDTH
-                        )
-                      }
-                      childImageSlideMobile: childImageSharp {
-                        fluid(maxWidth: 325, maxHeight: 250) {
-                          ...GatsbyImageSharpFluid_withWebp
-                        }
-                        gatsbyImageData(
-                          height: 250
-                          width: 325
-                          transformOptions: { cropFocus: CENTER }
-                          layout: FULL_WIDTH
-                        )
-                      }
-                      childImageSlideDesktop: childImageSharp {
-                        fluid(maxWidth: 450, maxHeight: 400) {
-                          ...GatsbyImageSharpFluid_withWebp
-                        }
-                        gatsbyImageData(
-                          height: 400
-                          width: 450
-                          transformOptions: { cropFocus: CENTER }
-                          layout: FULL_WIDTH
-                        )
-                      }
-                    }
+                    alt
                   }
-                  field_components {
-                    ... on component__text {
-                      relationships {
-                        component_type {
-                          name
-                        }
-                      }
-                      field_body {
-                        processed
+                  created(formatString: "MMM D, YYYY")
+                  path {
+                    alias
+                  }
+                  relationships {
+                    node_type {
+                      name
+                    }
+                    uid {
+                      name: display_name
+                    }
+                    field_e_book_file {
+                      filename
+                      id
+                      localFile {
+                        publicURL
                       }
                     }
-                    ... on component__image {
+                    field_image {
                       id
-                      field_image {
-                        alt
-                      }
-                      relationships {
-                        component_type {
-                          name
+                      localFile {
+                        publicURL
+                        childImageSharp {
+                          fluid(maxWidth: 530, maxHeight: 400) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                          gatsbyImageData(
+                            height: 400
+                            width: 530
+                            transformOptions: { cropFocus: CENTER }
+                            layout: FULL_WIDTH
+                          )
                         }
+                        childImageSlideMobile: childImageSharp {
+                          fluid(maxWidth: 325, maxHeight: 250) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                          gatsbyImageData(
+                            height: 250
+                            width: 325
+                            transformOptions: { cropFocus: CENTER }
+                            layout: FULL_WIDTH
+                          )
+                        }
+                        childImageSlideDesktop: childImageSharp {
+                          fluid(maxWidth: 450, maxHeight: 400) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                          gatsbyImageData(
+                            height: 400
+                            width: 450
+                            transformOptions: { cropFocus: CENTER }
+                            layout: FULL_WIDTH
+                          )
+                        }
+                      }
+                    }
+                    field_components {
+                      ... on component__text {
+                        relationships {
+                          component_type {
+                            name
+                          }
+                        }
+                        field_body {
+                          processed
+                        }
+                      }
+                      ... on component__image {
+                        id
                         field_image {
-                          id
-                          localFile {
-                            publicURL
-                            childImageSharp {
-                              fluid(maxWidth: 630, maxHeight: 630) {
-                                ...GatsbyImageSharpFluid_withWebp
+                          alt
+                        }
+                        relationships {
+                          component_type {
+                            name
+                          }
+                          field_image {
+                            id
+                            localFile {
+                              publicURL
+                              childImageSharp {
+                                fluid(maxWidth: 630, maxHeight: 630) {
+                                  ...GatsbyImageSharpFluid_withWebp
+                                }
                               }
                             }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                ... on case_study {
+                  id
+                  title
+                  field_subtitle
+                  field_inverse_header
+                  field_image_arrangement
+                  field_image {
+                    alt
+                  }
+                  field_secondary_image {
+                    alt
+                  }
+                  field_tertiary_image {
+                    alt
+                  }
+                  path {
+                    alias
+                  }
+                  relationships {
+                    field_tags {
+                      name
+                    }
+                    field_image {
+                      id
+                      localFile {
+                        publicURL
+                        childImageSharp {
+                          fluid(
+                            maxWidth: 850
+                            maxHeight: 850
+                            cropFocus: NORTH
+                          ) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                        }
+                        childImageMobile: childImageSharp {
+                          fixed(width: 335, height: 260, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeA: childImageSharp {
+                          fixed(width: 450, height: 320, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeB: childImageSharp {
+                          fixed(width: 380, height: 420, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeC: childImageSharp {
+                          fixed(width: 420, height: 340, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                      }
+                    }
+                    field_secondary_image {
+                      id
+                      localFile {
+                        publicURL
+                        childImageSharp {
+                          fluid(maxWidth: 850, maxHeight: 850) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                        }
+                        childImageMobile: childImageSharp {
+                          fixed(width: 1, height: 1) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeA: childImageSharp {
+                          fixed(width: 250, height: 180, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeB: childImageSharp {
+                          fixed(width: 340, height: 260, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeC: childImageSharp {
+                          fixed(width: 270, height: 210, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                      }
+                    }
+                    field_tertiary_image {
+                      id
+                      localFile {
+                        publicURL
+                        childImageSharp {
+                          fluid(maxWidth: 850, maxHeight: 850) {
+                            ...GatsbyImageSharpFluid_withWebp
+                          }
+                        }
+                        childImageMobile: childImageSharp {
+                          fixed(width: 1, height: 1) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeA: childImageSharp {
+                          fixed(width: 250, height: 495, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeB: childImageSharp {
+                          fixed(width: 230, height: 210, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
+                          }
+                        }
+                        childImageTypeC: childImageSharp {
+                          fixed(width: 320, height: 210, cropFocus: CENTER) {
+                            ...GatsbyImageSharpFixed_withWebp_noBase64
                           }
                         }
                       }
