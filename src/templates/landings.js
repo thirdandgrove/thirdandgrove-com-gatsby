f4bef7e4d76e08bfb6a4bdc04edc1be3633e23cc
RonaldChicaVera <rchica@rootstack.com>
2023-07-24 10:31:19 -0500

    T5-413: create scrolling logo component

--- src/templates/landings.js
+++ src/templates/landings.js
@@ -207,6 +207,49 @@ export const query = graphql`
               }
             }
           }
+          ... on component__scrolling_logos {
+            id
+            field_header_text
+            relationships {
+              component_type {
+                name
+              }
+              field_media_background {
+                id
+                localFile {
+                  publicURL
+                  childImageSharp {
+                    fluid(maxWidth: 800, cropFocus: CENTER) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                    squareImage: gatsbyImageData(
+                      width: 700
+                      height: 700
+                      transformOptions: { cropFocus: CENTER }
+                      layout: CONSTRAINED
+                    )
+                  }
+                }
+              }
+              field_logos {
+                id
+                localFile {
+                  publicURL
+                  childImageSharp {
+                    fluid(maxWidth: 800, cropFocus: CENTER) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                    squareImage: gatsbyImageData(
+                      width: 700
+                      height: 700
+                      transformOptions: { cropFocus: CENTER }
+                      layout: CONSTRAINED
+                    )
+                  }
+                }
+              }
+            }
+          }
           ... on component__icon_list_component {
             id
             field_header_text
