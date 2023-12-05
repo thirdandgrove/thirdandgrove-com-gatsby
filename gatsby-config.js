c7352b04cb342e3e750633a3f61e88acefc4f63f
naeluh <naeluh@gmail.com>
2023-12-05 15:09:47 -0800

    T5-0 Turn off fast builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,7 +166,6 @@ module.exports = {
           username: process.env.BASIC_AUTH_USERNAME,
           password: process.env.BASIC_AUTH_PASSWORD,
         },
-        fastBuilds: true,
       },
     },
     `gatsby-plugin-remove-serviceworker`,
