ad743d45fdd951f24073aaf20284628378091609
naeluh <naeluh@gmail.com>
2024-03-06 17:43:45 -0800

    T5-0 Add check for video section

--- src/components/ContentBody/FullWidthVideo.js
+++ src/components/ContentBody/FullWidthVideo.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-
 import VideoSection from '../VideoSection/VideoSection';
+import { useHasBeenVisible } from '../../hooks/useVisibility';
 
 const FullWidthVideo = ({ data }) => {
+  const halfPage = useRef();
+  const preload = useRef();
+  const hasScrolled = useHasBeenVisible(halfPage);
+  const isScrolling = useHasBeenVisible(preload);
   const mainVideo = data.relationships.field_main_video.localFile.publicURL;
   const autoPlayVideo =
     data.relationships.field_autoplay_video.localFile.publicURL;
 
-  return <VideoSection url={mainVideo} mp4={autoPlayVideo} isFile isContent />;
+  return hasScrolled || isScrolling ? (
+    <VideoSection url={mainVideo} mp4={autoPlayVideo} isFile isContent />
+  ) : (
+    <div ref={halfPage}></div>
+  );
 };
 
 FullWidthVideo.propTypes = {
